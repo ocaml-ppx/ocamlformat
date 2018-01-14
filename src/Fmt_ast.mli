207e98ba2deecd3c2612d01510a629af13dd151a
Josh Berdine <jjb@fb.com>
2018-01-14 23:20:11 +0000

    reformat

--- src/Fmt_ast.mli
+++ src/Fmt_ast.mli
@@ -16,9 +16,9 @@ module Format = Format_
 open Migrate_ast
 open Parsetree
 
+(** Raise this exception to Indicate that formatting has been disabled for
+    the current file *)
 exception Formatting_disabled
-    (** Raise this exception to Indicate that formatting has been disabled for the
-        current file *)
 
 val fmt_signature : Conf.t -> signature -> Fmt.t
 (** Format a signature. *)
