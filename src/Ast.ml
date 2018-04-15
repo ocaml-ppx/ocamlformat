063155560b726a0afe566eea0cc23262b4c58708
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-04-15 12:31:20 +0100

    small refactoring

--- src/Ast.ml
+++ src/Ast.ml
@@ -931,13 +931,8 @@ end = struct
       | Pexp_record (flds, _)
         when List.exists flds ~f:(fun (_, e0) -> e0 == exp) ->
           exposed Non_apply exp (* Non_apply is perhaps pessimistic *)
-      | Pexp_record
-          ( _
-          , Some
-              ( { pexp_desc=
-                    Pexp_apply ({pexp_desc= Pexp_ident {txt= Lident i}}, _)
-                } as e0 ) )
-        when (e0 == exp) && is_prefix_id i ->
+      | Pexp_record (_, Some ({pexp_desc= Pexp_apply (ident, [_])} as e0))
+        when (e0 == exp) && is_prefix ident ->
           (* don't put parens around [!e] in [{ !e with a; b }] *)
           false
       | Pexp_record (_, Some ({pexp_desc= Pexp_apply _} as e0))
