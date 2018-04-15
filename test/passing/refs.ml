1c73364a5f1d8fff0d951d43325169ca02eea5eb
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-04-15 11:54:16 +0100

    regtests: add regtests for refs

---
+++ test/passing/refs.ml
@@ -0,0 +1,10 @@
+let _ =
+  x := 2 ;
+  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  := 2 ;
+  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  := something very
+       looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong ;
+  xx
+  := something very
+       loooooooooooooooooooooooooooooooooooooooooooooooooooooooong
