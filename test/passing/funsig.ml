24a98ed6d4113c431744abb1f8065aeedaf741aa
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-04-17 14:11:47 +0100

    regtests: add more tests

---
+++ test/passing/funsig.ml
@@ -0,0 +1,30 @@
+external ident : a -> b -> c -> d = "something"
+
+external ident : a -> b -> c -> d = "something" "else"
+
+val ident : a -> b -> c -> d
+
+val ident
+  :  arg1_is_loooooooooooooooooooooooooooooooong
+  -> arg2
+  -> arg3
+  -> arg4
+
+external ident
+  :  arg1_is_loooooooooooooooooooooooooooooooong
+  -> arg2
+  -> arg3
+  -> arg4
+  = "something" "else"
+
+type t = {f1: a -> b -> c; f2: int; f3: a -> b -> c -> d -> e}
+
+type t =
+  { f1: a -> b -> c
+  ; f2: int
+  ; f3
+      :  a_is_loooooooooooooooooooooooooooooooong
+      -> b_is_loooooooooooooooooooooooooooooooong
+      -> c
+      -> d
+      -> e }
