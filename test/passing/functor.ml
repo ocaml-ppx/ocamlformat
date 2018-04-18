39eeec3367bc76eca39c2d9a8ba2c268f34d7854
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-04-18 10:53:16 +0100

    regtests: udpate

--- test/passing/functor.ml
+++ test/passing/functor.ml
@@ -1,5 +1,16 @@
-module type S = functor () -> sig end
+module type S = functor () -> sig
+  
+end
 
-module type S = functor () -> functor () -> sig end
+module type S = functor () -> functor () -> sig
+  
+end
 
-module M : functor () -> sig end = functor () -> struct end
+module M : functor () -> sig
+  
+end =
+functor
+() ->
+struct
+  
+end
