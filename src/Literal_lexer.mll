4cc7050eb9791187f4011942bdab9bacdeb359a6
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-04-23 09:09:52 +0100

    missing headers

--- src/Literal_lexer.mll
+++ src/Literal_lexer.mll
@@ -1,4 +1,15 @@
 {
+(**********************************************************************
+ *                                                                    *
+ *                            OCamlFormat                             *
+ *                                                                    *
+ *  Copyright (c) 2017-present, Facebook, Inc.  All rights reserved.  *
+ *                                                                    *
+ *  This source code is licensed under the MIT license found in the   *
+ *  LICENSE file in the root directory of this source tree.           *
+ *                                                                    *
+ **********************************************************************)
+
 open Caml
 
 (* To buffer string literals *)
--- src/Source.mli
+++ src/Source.mli
@@ -1,3 +1,14 @@
+(**********************************************************************
+ *                                                                    *
+ *                            OCamlFormat                             *
+ *                                                                    *
+ *  Copyright (c) 2017-present, Facebook, Inc.  All rights reserved.  *
+ *                                                                    *
+ *  This source code is licensed under the MIT license found in the   *
+ *  LICENSE file in the root directory of this source tree.           *
+ *                                                                    *
+ **********************************************************************)
+
 val init : string -> unit
 
 val string_between : Location.t -> Location.t -> string option
