b43008ed8e139da7836e2cea7c236afeebc54bdc
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-05-06 23:03:02 +0100

    Regtests
    regtests, fixup

--- test/passing/attributes.ml
+++ test/passing/attributes.ml
@@ -53,3 +53,13 @@ module type M = sig
 
   module T = T [@@test8]
 end
+
+let f = (fun [@inline] [@inline never] x -> x)
+
+let g = (fun [@inline] [@something_else] [@ocaml.inline] x -> x)
+
+let h x = (g [@inlined] [@ocaml.inlined never]) x
+
+let v = (fun [@inline] [@inlined] x -> x) 1
+
+let i = (fun [@inline] x -> x)[@@inline]
