cb5c8f57db1c89e1149a3bf71684b280629b43ca
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-05-07 03:10:43 +0100

    Fix: no short form of extension with attribs

--- src/Fmt_ast.ml
+++ src/Fmt_ast.ml
@@ -1745,7 +1745,8 @@ and fmt_expression c ?(box= true) ?epi ?eol ?parens ?ext
                             ( Pexp_while _ | Pexp_for _ | Pexp_match _
                             | Pexp_try _ | Pexp_let _ | Pexp_ifthenelse _
                             | Pexp_sequence _ | Pexp_new _
-                            | Pexp_letmodule _ | Pexp_object _ ) } as e1 )
+                            | Pexp_letmodule _ | Pexp_object _ )
+                        ; pexp_attributes= [] } as e1 )
                     , _ ) } as str ) ] ) ->
       hvbox 0
         ( fmt_expression c ~box ?eol ~parens ~ext (sub_exp ~ctx:(Str str) e1)
