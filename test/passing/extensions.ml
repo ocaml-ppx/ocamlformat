fb740567ac918bdf22df2a40604b2c864b20ef4f
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-05-09 00:43:53 +0100

    Fix: short syntax for extensions

--- test/passing/extensions.ml
+++ test/passing/extensions.ml
@@ -2,6 +2,12 @@ let () = [%ext expr] ; ()
 
 let _ = (match%ext x with () -> ()) [@attr y]
 
+let _ =
+  match%ext x with () ->
+    let y = [%test let x = y] in
+    let%test x = d in
+    d
+
 val f : compare:[%compare : 'a] -> sexp_of:[%sexp_of : 'a] -> t
 
 let invariant t =
