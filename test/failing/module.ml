2058a00ae439175a8accf35e8d02ae0ec84956c2
hhugo <hugo.heuzard@gmail.com>
2018-05-09 01:23:00 +0100

    Regtest: bad formatting for let module (#192)

---
+++ test/failing/module.ml
@@ -0,0 +1,10 @@
+module AAAAAAAAAAAAAAAAAAA =
+  Soooooooooooooooooooooooome.Loooooooooooooooooooooooong.Mod
+
+
+let _ =
+  let module A = B in
+  let module AAAAAAAAAAAAAAAAAAA =
+    Soooooooooooooooooooooooome.Loooooooooooooooooooooooong.Mod
+  in
+  t
