dcbaa708de278cdca2aba410aa2875d9265633f4
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-06-25 11:57:32 +0100

    Improve: more tuning for functors

--- test/passing/functor.ml
+++ test/passing/functor.ml
@@ -4,17 +4,22 @@ module type S = functor () () -> sig end
 
 module type M = functor () -> sig end
 
-module type M = functor (S :  S) -> sig end
+module type M = functor (S : S) -> sig end
 
-module type M = functor (S :  S) (T :  T) -> sig end
+module type M = functor (S : S) (T : T) -> sig end
 
-module type M = functor (S :  S) (T :  T) -> U
+module type M = functor (S : S) (T : T) -> U
 
-module type M = functor (S :  S) () -> sig end
+module type M = functor (S : S) () -> sig end
 
-module type M = functor (SSSSS :  SSSSSSSSSSSSSS)
-(TTTTT :  TTTTTTTTTTTTTTTT)
--> sig end
+module type M = functor
+  (SSSSS : SSSSSSSSSSSSSS)
+  (TTTTT : TTTTTTTTTTTTTTTT)
+  -> sig
+  val t1 : a
+
+  val t2 : b
+end
 
 module M : functor () -> sig end = functor () -> struct end
 
@@ -30,9 +35,13 @@ module M = (functor (S : S) (T : T) -> (struct end : U)) (S) (T)
 
 module M =
   (functor
-  (SSSSS : sssssSSSSSSSSSSSSSS)
-  (TTTTT : TTTTTTTTTTTTTTTTTTTTT) ->
-  struct
-  end)
+    (SSSSS : sssssSSSSSSSSSSSSSS)
+    (TTTTT : TTTTTTTTTTTTTTTTTTTTT)
+    ->
+    struct
+      let x = 2
+
+      let y = 3
+    end)
     (S)
     (T)
