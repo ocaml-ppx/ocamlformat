04efd06b0479cbd08e4d096fae45b9019e567116
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-06-25 11:28:12 +0100

    Regtest: add tests for functors

--- test/passing/functor.ml
+++ test/passing/functor.ml
@@ -2,6 +2,20 @@ module type S = functor () -> sig end
 
 module type S = functor () () -> sig end
 
+module type M = functor () -> sig end
+
+module type M = functor (S :  S) -> sig end
+
+module type M = functor (S :  S) (T :  T) -> sig end
+
+module type M = functor (S :  S) (T :  T) -> U
+
+module type M = functor (S :  S) () -> sig end
+
+module type M = functor (SSSSS :  SSSSSSSSSSSSSS)
+(TTTTT :  TTTTTTTTTTTTTTTT)
+-> sig end
+
 module M : functor () -> sig end = functor () -> struct end
 
 module M = (functor (S : S) -> struct end) (S)
@@ -13,3 +27,12 @@ module M = (functor (S : S) (T : T) -> struct end : U) (S) (T)
 module M = (functor (S : S) () -> struct end : U) (S) (T)
 
 module M = (functor (S : S) (T : T) -> (struct end : U)) (S) (T)
+
+module M =
+  (functor
+  (SSSSS : sssssSSSSSSSSSSSSSS)
+  (TTTTT : TTTTTTTTTTTTTTTTTTTTT) ->
+  struct
+  end)
+    (S)
+    (T)
