24f039498b933bbe50074c05443571d48bcb024f
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-07-04 20:47:30 +0100

    Regtests: udpate

--- test/passing/comments_in_record.ml
+++ test/passing/comments_in_record.ml
@@ -1,18 +1,14 @@
-(* - [b: float] gets misaligned in the first case.
-
-   - comments and doc comments behave differently. *)
-
 type t =
   { a: int
-  ; (* some comment *)
-    b: float
+  (* some comment *)
+  ; b: float
   ; c: string
   ; d: [`something_looooooooooooooooooooooooooooooooong] }
 
 type t =
   { a: int
-  ; (** some comment *)
-    b: float
+  (** some comment *)
+  ; b: float
   ; c: string
   ; d: [`something_looooooooooooooooooooooooooooooooong] }
 
@@ -25,6 +21,6 @@ let { (* cmts *)
     ; d=
       (* d *)
       (D: loooooooooooooooooooooooooooooooooooooooooooooooooooooooong_int)
-    ; (* d *)
-      e : loooooooooooooooooooooooooooooooooooooooooooooooooooooooong_int } =
+        (* d *)
+    ; e : loooooooooooooooooooooooooooooooooooooooooooooooooooooooong_int } =
   exp
