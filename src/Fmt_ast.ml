71d306c50d5c27bbd714c40fcc879fdf20010658
Josh Berdine <jjb@fb.com>
2018-07-25 16:06:22 +0100

    fix #229

--- src/Fmt_ast.ml
+++ src/Fmt_ast.ml
@@ -1203,39 +1203,43 @@ and fmt_expression c ?(box= true) ?epi ?eol ?parens ?ext
         ; pexp_attributes= [] }
       , [(Nolabel, s); (Nolabel, i)] ) ->
       wrap_if parens "(" ")"
-        ( fmt_expression c (sub_exp ~ctx s)
-        $ fmt ".("
-        $ fmt_expression c (sub_exp ~ctx i)
-        $ fmt ")" )
+        (hovbox 0
+           ( fmt_expression c (sub_exp ~ctx s)
+           $ fmt ".("
+           $ fmt_expression c (sub_exp ~ctx i)
+           $ fmt ")" ))
   | Pexp_apply
       ( { pexp_desc= Pexp_ident {txt= Ldot (Lident "String", "get")}
         ; pexp_attributes= [] }
       , [(Nolabel, s); (Nolabel, i)] ) ->
       wrap_if parens "(" ")"
-        ( fmt_expression c (sub_exp ~ctx s)
-        $ fmt ".["
-        $ fmt_expression c (sub_exp ~ctx i)
-        $ fmt "]" )
+        (hovbox 0
+           ( fmt_expression c (sub_exp ~ctx s)
+           $ fmt ".["
+           $ fmt_expression c (sub_exp ~ctx i)
+           $ fmt "]" ))
   | Pexp_apply
       ( { pexp_desc= Pexp_ident {txt= Ldot (Lident "Array", "set")}
         ; pexp_attributes= [] }
       , [(Nolabel, s); (Nolabel, i); (Nolabel, e)] ) ->
       wrap_if parens "(" ")"
-        ( fmt_expression c (sub_exp ~ctx s)
-        $ fmt ".("
-        $ fmt_expression c (sub_exp ~ctx i)
-        $ fmt ")@ <- "
-        $ fmt_expression c (sub_exp ~ctx e) )
+        (hovbox 0
+           ( fmt_expression c (sub_exp ~ctx s)
+           $ fmt ".("
+           $ fmt_expression c (sub_exp ~ctx i)
+           $ fmt ")@ <- "
+           $ fmt_expression c (sub_exp ~ctx e) ))
   | Pexp_apply
       ( { pexp_desc= Pexp_ident {txt= Ldot (Lident "String", "set")}
         ; pexp_attributes= [] }
       , [(Nolabel, s); (Nolabel, i); (Nolabel, e)] ) ->
       wrap_if parens "(" ")"
-        ( fmt_expression c (sub_exp ~ctx s)
-        $ fmt ".["
-        $ fmt_expression c (sub_exp ~ctx i)
-        $ fmt "]@ <- "
-        $ fmt_expression c (sub_exp ~ctx e) )
+        (hovbox 0
+           ( fmt_expression c (sub_exp ~ctx s)
+           $ fmt ".["
+           $ fmt_expression c (sub_exp ~ctx i)
+           $ fmt "]@ <- "
+           $ fmt_expression c (sub_exp ~ctx e) ))
   | Pexp_apply
       ( {pexp_desc= Pexp_ident {txt= Lident ":="}; pexp_attributes= []}
       , [(Nolabel, r); (Nolabel, v)] )
