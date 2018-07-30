9ebbba244cd8a4bdbe993ff61ce3fbd62e2d7f19
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-07-30 07:27:29 +0100

    Fix: fix string_literal with when its location include its attribute.

--- test/failing-output/attribute_and_expression.ml
+++ test/failing-output/attribute_and_expression.ml
@@ -1,1 +1,3 @@
-NON-ZERO EXIT CODE
+ocamlformat.exe: Cannot process "failing/attribute_and_expression.ml".
+  Please report this bug at https://github.com/ocaml-ppx/ocamlformat/issues.
+ocamlformat.exe: BUG: formatting changed ast.
