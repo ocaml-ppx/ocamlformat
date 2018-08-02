82f2a4fffe2e207162ad83160a99ec5cc32888ad
Josh Berdine <jjb@fb.com>
2018-08-03 00:04:53 +0100

    update tests

--- test/failing/module.ml
+++ test/failing/module.ml
@@ -1,7 +1,6 @@
 module AAAAAAAAAAAAAAAAAAA =
   Soooooooooooooooooooooooome.Loooooooooooooooooooooooong.Mod
 
-
 let _ =
   let module A = B in
   let module AAAAAAAAAAAAAAAAAAA =
--- test/passing/shortcut_ext_attr.ml
+++ test/passing/shortcut_ext_attr.ml
@@ -78,7 +78,7 @@ module M = (functor [@foo] (M : S) -> (val x) [@foo] (struct end [@foo]))
 module type S = functor [@foo1]
   (M : S)
   -> functor
-  (_ :(module type of  M) [@foo2])
+  (_ : (module type of M) [@foo2])
   -> sig end [@foo3]
 
 (* Structure items *)
