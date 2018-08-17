35b54bcfbbcdec1f27ef6a6da92238302fe7b92d
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-08-17 10:41:56 +0100

    add missing file

---
+++ test/passing/module.ml
@@ -0,0 +1,9 @@
+module AAAAAAAAAAAAAAAAAAA =
+  Soooooooooooooooooooooooome.Loooooooooooooooooooooooong.Mod
+
+let _ =
+  let module A = B in
+  let module AAAAAAAAAAAAAAAAAAA =
+    Soooooooooooooooooooooooome.Loooooooooooooooooooooooong.Mod
+  in
+  t
