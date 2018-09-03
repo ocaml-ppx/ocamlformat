9f49da17029bdf170cc0a02556b6b6b684251661
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-09-03 13:54:23 +0800

    fix comment

--- src/Ast.mli
+++ src/Ast.mli
@@ -151,8 +151,8 @@ val exposed_right_exp : cls -> expression -> bool
     of [exp] which is of class [cls] and is not parenthesized. *)
 
 val exposed_left_exp : expression -> bool
-(** [exposed_right_exp cls exp] holds if there is a right-most subexpression
-    of [exp] which is of class [cls] and is not parenthesized. *)
+(** [exposed_left_exp exp] holds if the left-most subexpression
+    of [exp] is a prefix operators. *)
 
 val prec_ast : t -> prec option
 (** [prec_ast ast] is the precedence of [ast]. Meaningful for binary
