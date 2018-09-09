c0fe1149106b8313e4dc57571d25f9a1002964d6
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-09-09 09:55:20 +0100

    test

--- test/passing/doc_comments.ml
+++ test/passing/doc_comments.ml
@@ -12,17 +12,3 @@ include A
 type t = C of int  (** docstring comment *)
 
 type t = C of int [@ocaml.doc " docstring attribute "]
-
-(** comment *)
-include Mod
-
-(** before *)
-let x = 2
-(** after *)
-
-(**floatting1*)
-(**floatting2*)
-
-(**before*)
-and y = 2
-(** after *)
