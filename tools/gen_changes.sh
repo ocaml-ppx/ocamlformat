8e6e40475c7816962fbe82afd81d5889928eba3e
Josh Berdine <jjb@fb.com>
2018-10-24 08:51:44 +0100

    Admin: add code of conduct and copyright headers to build and package system

--- tools/gen_changes.sh
+++ tools/gen_changes.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
+######################################################################
+#                                                                    #
+#                            OCamlFormat                             #
+#                                                                    #
+#  Copyright (c) 2017-present, Facebook, Inc.  All rights reserved.  #
+#                                                                    #
+#  This source code is licensed under the MIT license found in the   #
+#  LICENSE file in the root directory of this source tree.           #
+#                                                                    #
+######################################################################
+
 git log --no-merges --pretty=format:"  + %s (%an)" $1..$2
--- tools/gen_version.sh
+++ tools/gen_version.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+######################################################################
+#                                                                    #
+#                            OCamlFormat                             #
+#                                                                    #
+#  Copyright (c) 2017-present, Facebook, Inc.  All rights reserved.  #
+#                                                                    #
+#  This source code is licensed under the MIT license found in the   #
+#  LICENSE file in the root directory of this source tree.           #
+#                                                                    #
+######################################################################
+
 # usage: gen_version.sh <file> [<version>]
 
 FILE="$1"
