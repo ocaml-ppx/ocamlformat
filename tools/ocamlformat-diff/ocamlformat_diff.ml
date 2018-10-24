65fb4fb2f128124ce7a3aa3a7da6f86eef7c0c29
Guillaume Petiot <guillaume@tarides.com>
2018-10-24 09:20:22 +0200

    Improve: add license header for tools/ocamlformat-diff/ocamlformat_diff.ml (#466)

--- tools/ocamlformat-diff/ocamlformat_diff.ml
+++ tools/ocamlformat-diff/ocamlformat_diff.ml
@@ -1,3 +1,14 @@
+(**********************************************************************
+ *                                                                    *
+ *                            OCamlFormat                             *
+ *                                                                    *
+ *  Copyright (c) 2017-present, Facebook, Inc.  All rights reserved.  *
+ *                                                                    *
+ *  This source code is licensed under the MIT license found in the   *
+ *  LICENSE file in the root directory of this source tree.           *
+ *                                                                    *
+ **********************************************************************)
+
 open Rresult
 
 (** [run_ocamlformat cmd ~ctx ~file_in] runs the ocamlformat command [cmd]
