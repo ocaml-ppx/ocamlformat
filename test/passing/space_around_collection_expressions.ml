5b18612440a81ba41cdb988e1d72a21c94b4a664
Jules Aguillon <juloo.dsi@gmail.com>
2019-01-30 18:56:35 +0100

    Add tests

---
+++ test/passing/space_around_collection_expressions.ml
@@ -0,0 +1,20 @@
+[@@@ocamlformat "type-decl=sparse"]
+
+type t =
+  { x: int
+  ; y: int }
+
+type t =
+  [ `X
+  | `Y ]
+
+[@@@ocamlformat "space-around-collection-expressions"]
+
+type t =
+  { x: int
+  ; y: int
+  }
+
+type t =
+  [ `X
+  | `Y ]
