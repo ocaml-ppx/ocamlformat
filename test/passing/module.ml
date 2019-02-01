13937c784e7b5e6019fc4a0f074c9f3c2875577a
Jules Aguillon <juloo.dsi@gmail.com>
2019-02-01 16:08:23 +0100

    Add tests

--- test/passing/module.ml
+++ test/passing/module.ml
@@ -28,3 +28,82 @@ module C = struct
 
   and B : sig end = B
 end
+
+module M :
+  S
+  with type long_long_type = t
+   and type long_long_long_type = u
+   and type very_long_long_type = t =
+  M
+
+module type X = sig
+  val n : (module S with type t = t)
+
+  val o :
+    (module
+     S
+       with type long_long_type = t
+        and type long_long_long_type = u
+        and type very_long_long_type = t)
+end
+
+module X = (val X.n : S with type t = t)
+
+module Y = ( val X.o
+               : S
+               with type long_long_type = t
+                and type long_long_long_type = u
+                and type very_long_long_type = t )
+
+let _ =
+  (module M
+  : S
+    with type long_long_type = t
+     and type long_long_long_type = u
+     and type very_long_long_type = t )
+
+let _ =
+  ( module struct
+    type t
+  end
+  : S
+    with type t = t )
+
+let _ =
+  ( module struct
+    type t
+  end
+  : S )
+
+let _ =
+  ( module struct
+    type t
+  end
+  : Loooooooooooooooonnnnnnnnnnnnnnnng.Looooooooooooooonnnnnnnnnnnnnng.Mod
+  )
+
+let _ =
+  ( module struct
+    type t
+  end
+  : Loooooooooooooooonnnnnnnnnnnnnnnng.Looooooooooooooonnnnnnnnnnnnnng.Mod
+    with type looooooooonnnnnnng_type = t and type not_long_type = t' )
+
+let _ =
+  ( module struct
+    (* a *)
+    type t
+  end
+  (* b *)
+  : S
+  (* c *)
+    with type t = t (* d *) )
+
+let _ =
+  ( module struct
+    type t
+  end
+  : S
+    with type long_long_type = t
+     and type long_long_long_type = u
+     and type very_long_long_type = t )
