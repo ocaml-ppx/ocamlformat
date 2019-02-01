5bd33a556597bd40f9a320e6b2b27d6d1d1d05e1
Guillaume Petiot <guillpetiot@gmail.com>
2019-02-01 18:23:43 +0700

    forgot some files

---
+++ bin/Cli.mli
@@ -0,0 +1,13 @@
+(**********************************************************************
+ *                                                                    *
+ *                            OCamlFormat                             *
+ *                                                                    *
+ *  Copyright (c) 2017-present, Facebook, Inc.  All rights reserved.  *
+ *                                                                    *
+ *  This source code is licensed under the MIT license found in the   *
+ *  LICENSE file in the root directory of this source tree.           *
+ *                                                                    *
+ **********************************************************************)
+
+val action : Ocamlformat_api.Conf.action
+(** Formatting action: input type and source, and output destination. *)
