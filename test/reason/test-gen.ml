f0af1f3a6d2dbd33dc94c441701cb839dd6b12bf
hhugo <hugo.heuzard@gmail.com>
2019-02-11 01:45:42 +0800

    Improve reason support (#608)
    Sanitise ast parsed from reason files before formatting it. In particular,
    * tune the ast in presence of "explicit_arity" (previously done inside `norm`)
    * remove special reason attributes

---
+++ test/reason/test-gen.ml
@@ -0,0 +1,16 @@
+(* test *)
+
+let x = 3
+
+type schoolPerson = Teacher | Director | Student of string
+
+let greeting person =
+  match person with
+  | Teacher -> "Hey Professor!"
+  | Director -> "Hello Director."
+  | Student "Richard" -> "Still here Ricky?"
+  | Student anyOtherName -> "Hey, " ^ anyOtherName ^ "."
+
+let x = "\255"
+
+let x = '\255'
