027935af6954ecd3e821dd8935b498d50a5e792d
Guillaume Petiot <guillpetiot@gmail.com>
2019-02-11 23:55:31 +0700

    add test

---
+++ test/passing/wrap_comments.ml
@@ -0,0 +1,5 @@
+[@@@ocamlformat "wrap-comments=true"]
+
+type t =
+  | Aaaaaaaaaa (* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. *)
+  | Bbbbbbbbbb
