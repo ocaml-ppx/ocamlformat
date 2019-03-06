391ad14d772c99022039c3a4d8be0ec36dd1a750
Guillaume Petiot <guillpetiot@gmail.com>
2019-03-06 22:13:38 +0700

    test for space-around-collection-expressions=true

--- test/passing/list_pattern.ml
+++ test/passing/list_pattern.ml
@@ -19,3 +19,27 @@ let x = function
       (* "exercitation ullamco laboris nisi ut aliquip ex ea commodo " *)
      ] ->
       ()
+
+[@@@ocamlformat "space-around-collection-expressions=true"]
+
+let x = function
+  | [ "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+    ; [ "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      ; (* ", sed do eiusmod tempor incididunt ut labore et dolore"; *)
+        "sed do eiusmod tempor incididunt ut labore et dolore"
+        (* " magna aliqua. Ut enim ad minim veniam, quis nostrud "; *)
+        (* "exercitation ullamco laboris nisi ut aliquip ex ea commodo " *)
+      ]
+    ; (* ", sed do eiusmod tempor incididunt ut labore et dolore"; *)
+      "sed do eiusmod tempor incididunt ut labore et dolore"
+      (* " magna aliqua. Ut enim ad minim veniam, quis nostrud "; *)
+      (* "exercitation ullamco laboris nisi ut aliquip ex ea commodo " *)
+    ] ->
+      ()
+  | [ [ "Lorem ipsum dolor sit amet, consectetur adipiscing elit" ]
+    ; (* ", sed do eiusmod tempor incididunt ut labore et dolore"; *)
+      "sed do eiusmod tempor incididunt ut labore et dolore"
+      (* " magna aliqua. Ut enim ad minim veniam, quis nostrud "; *)
+      (* "exercitation ullamco laboris nisi ut aliquip ex ea commodo " *)
+    ] ->
+      ()
