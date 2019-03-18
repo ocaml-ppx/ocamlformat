14802fa79b78d2a22e40e9f6b4084aa928024e63
Guillaume Petiot <guillpetiot@gmail.com>
2019-03-18 21:07:23 +0700

    add test

--- test/passing/expect_test.ml
+++ test/passing/expect_test.ml
@@ -1,3 +1,16 @@
 let%expect_test _ = e
 
 let%bench "test" = fun () -> ()
+
+let%expect_test _ =
+  assert false ;
+  [%expect.unreachable]
+  [@@expect.uncaught_exn
+    {|
+      (* CR expect_test_collector: This test expectation appears to contain a backtrace.
+         This is strongly discouraged as backtraces are fragile.
+         Please change this test to not include a backtrace. *)
+
+      "Assert_failure test.ml:5:6"
+      Raised at file "test.ml", line 4, characters 6-18
+      Called from file "collector/expect_test_collector.ml", line 225, characters 12-19 |}]
