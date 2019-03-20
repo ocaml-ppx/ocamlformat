4398ed8d200d3ff51f08d46abed2999ec6723740
Guillaume Petiot <guillpetiot@gmail.com>
2019-03-20 13:35:39 +0700

    add test

---
+++ test/passing/wrap_comments_break.ml
@@ -0,0 +1,8 @@
+let _ =
+  let _ =
+    fffffffffff
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      bbbbbbbbbbbbbbbbbbbbbbbbbb
+      ~f:(fun x -> return xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx)
+  in
+  2
