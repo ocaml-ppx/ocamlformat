ocaml-ppx/ocamlformat

commit bf163eb70567349e2b1c0afa41d9ecccdfea9de0

Fix break before parens no wrap fun args (#690)

src/Conf.ml
@@ -1666,7 +1666,8 @@ let read_config_file conf filename_kind =
                       ("unknown option", Sexp.Atom name)
                   | `Bad_value (name, reason) ->
                       ( "bad value for"
-                      , Sexp.List [Sexp.Atom name; Sexp.Atom reason] ) )) )
+                      , Sexp.List [Sexp.Atom name; Sexp.Atom reason] ) ))
+      )
     with Sys_error _ -> conf )
 
 let update_using_env conf =
@@ -1807,8 +1808,7 @@ let action =
   if !inplace then
     Inplace
       (List.map !inputs ~f:(fun file ->
-           {kind= kind_of file; name= file; file; conf= build_config ~file}
-       ))
+           {kind= kind_of file; name= file; file; conf= build_config ~file} ))
   else if !check then
     Check
       (List.map !inputs ~f:(fun file ->

src/Fmt_ast.ml
@@ -1503,7 +1503,14 @@ and fmt_expression c ?(box = true) ?pro ?epi ?eol ?parens ?(indent_wrap = 0)
                              | _ -> None )
                            xbody )
                    $ fmt_or_k c.conf.indicate_multiline_delimiters
-                       (fits_breaks ")" "@ )") (fits_breaks ")" "@,)") )
+                       (fmt_or_k
+                          (Location.is_single_line pexp_loc c.conf.margin)
+                          (fmt ")")
+                          (fmt_or_k
+                             (Location.is_single_line xbody.ast.pexp_loc
+                                c.conf.margin)
+                             (fmt " )") (fits_breaks "@ )" "@ )")))
+                       (fmt ")") )
                $ fmt_atrs ))
       | ( lbl
         , ( { pexp_desc= Pexp_function [{pc_lhs; pc_guard= None; pc_rhs}]
@@ -2226,8 +2233,8 @@ and fmt_class_structure c ~ctx ?ext self_ fields =
         ( fmt "object"
         $ fmt_extension_suffix c ext
         $ opt self_ (fun self_ ->
-              fmt "@;" $ wrap "(" ")" (fmt_pattern c (sub_pat ~ctx self_))
-          ) )
+              fmt "@;" $ wrap "(" ")" (fmt_pattern c (sub_pat ~ctx self_)) )
+        )
     $ cmts_after_self
     $ ( match fields with
       | ({pcf_desc= Pcf_attribute a}, _) :: _
@@ -2790,7 +2797,8 @@ and fmt_type_declaration c ?(pre = "") ?(suf = ("" : _ format)) ?(brk = suf)
                         | `Sparse -> "@;<1000 0>; "
                         | `Compact -> "@,; " )
                       $ fmt_label_declaration c ctx x ~last
-                      $ fmt_if (last && exposed_right_typ x.pld_type) " " )))
+                      $ fmt_if (last && exposed_right_typ x.pld_type) " "
+                  )))
       | `After ->
           box_manifest
             ( fmt_manifest ~priv:Public mfst

src/Translation_unit.ml
@@ -285,8 +285,7 @@ let print_error ?(quiet_unstable = false) ?(quiet_comments = false)
           | _ -> () ) ;
           if Conf.debug then
             List.iter l ~f:(fun (msg, sexp) ->
-                Format.fprintf fmt "  %s: %s\n%!" msg (Sexp.to_string sexp)
-            )
+                Format.fprintf fmt "  %s: %s\n%!" msg (Sexp.to_string sexp) )
       | exn ->
           Format.fprintf fmt
             "  BUG: unhandled exception. Use [--debug] for details.\n%!" ;
@@ -356,8 +355,7 @@ let format xunit (conf : Conf.t) ?output_file ~input_name ~source ~parsed ()
                 ; ("old ast", old_ast)
                 ; ("new ast", new_ast) ]
                 |> List.filter_map ~f:(fun (s, f_opt) ->
-                       Option.map f_opt ~f:(fun f -> (s, String.sexp_of_t f))
-                   )
+                       Option.map f_opt ~f:(fun f -> (s, String.sexp_of_t f)) )
               in
               internal_error (`Doc_comment docstrings) args
             else
@@ -367,8 +365,7 @@ let format xunit (conf : Conf.t) ?output_file ~input_name ~source ~parsed ()
                 ; ("old ast", old_ast)
                 ; ("new ast", new_ast) ]
                 |> List.filter_map ~f:(fun (s, f_opt) ->
-                       Option.map f_opt ~f:(fun f -> (s, String.sexp_of_t f))
-                   )
+                       Option.map f_opt ~f:(fun f -> (s, String.sexp_of_t f)) )
               in
               internal_error `Ast_changed args ) ;
           (* Comments not preserved ? *)

src/Ast.ml
@@ -1292,8 +1292,8 @@ end = struct
       match str.pstr_desc with
       | Pstr_eval (e0, _) -> assert (e0 == exp)
       | Pstr_value (_, bindings) ->
-          assert (List.exists bindings ~f:(fun {pvb_expr} -> pvb_expr == exp)
-          )
+          assert (
+            List.exists bindings ~f:(fun {pvb_expr} -> pvb_expr == exp) )
       | Pstr_extension ((_, ext), _) -> assert (check_extensions ext)
       | Pstr_primitive _ | Pstr_type _ | Pstr_typext _ | Pstr_exception _
        |Pstr_module _ | Pstr_recmodule _ | Pstr_modtype _ | Pstr_open _
@@ -2001,7 +2001,7 @@ end = struct
             ({pexp_desc= Pexp_ident {txt= Lident i}}, _ :: (_, e2) :: _)
           when e2 == exp && is_infix_id i
                && Option.value_map ~default:false (prec_ast ctx)
-                    ~f:(fun p -> Poly.(p < Apply) ) ->
+                    ~f:(fun p -> Poly.(p < Apply)) ->
             true
         | Pexp_tuple e1N -> List.last_exn e1N == xexp.ast
         | _ -> false

src/Cmts.ml
@@ -443,7 +443,7 @@ let relocate t ~src ~before ~after =
       Option.iter (Hashtbl.find_and_remove tbl src) ~f:(fun src_data ->
           Hashtbl.update tbl dst ~f:(fun dst_data ->
               Option.fold dst_data ~init:src_data
-                ~f:(fun src_data dst_data -> f src_data dst_data ) ) )
+                ~f:(fun src_data dst_data -> f src_data dst_data) ) )
     in
     if Conf.debug then
       Format.eprintf "relocate %a to %a and %a@\n%!" Location.fmt src

test/passing/issue289.ml
@@ -37,31 +37,31 @@ let foo =
       ~doc:"Toy ID."
       ~args:[]
       ~typ:(non_null guid)
-      ~resolve:(fun _ctx x -> x.id )
+      ~resolve:(fun _ctx x -> x.id)
   ; field
       "name"
       ~doc:"Toy name."
       ~args:[]
       ~typ:(non_null string)
-      ~resolve:(fun _ctx x -> x.name )
+      ~resolve:(fun _ctx x -> x.name)
   ; field
       "description"
       ~doc:"Toy description."
       ~args:[]
       ~typ:string
-      ~resolve:(fun _ctx x -> x.description |> Util.option_of_string )
+      ~resolve:(fun _ctx x -> x.description |> Util.option_of_string)
   ; field
       "type"
       ~doc:"Toy type. Possible values are: car, animal, train."
       ~args:[]
       ~typ:(non_null toy_type_enum)
-      ~resolve:(fun _ctx x -> x.toy_type )
+      ~resolve:(fun _ctx x -> x.toy_type)
   ; field
       "createdAt"
       ~doc:"Date created."
       ~args:[]
       ~typ:(non_null Scalar.date_time)
-      ~resolve:(fun _ctx x -> x.created_at ) ]
+      ~resolve:(fun _ctx x -> x.created_at) ]
 
 [@@@ocamlformat "wrap-fun-args=true"]
 
@@ -86,14 +86,14 @@ let foo =
       | AAAAAAAAAAAAAAAAAAAa -> x.idddddddddddddddddddddddddd
       | BBBBBBBBBBBBBBBB -> ccccccccccccccccccccccc )
   ; field "id" ~doc:"Toy ID." ~args:[] ~typ:(non_null guid)
-      ~resolve:(fun _ctx x -> x.id )
+      ~resolve:(fun _ctx x -> x.id)
   ; field "name" ~doc:"Toy name." ~args:[] ~typ:(non_null string)
-      ~resolve:(fun _ctx x -> x.name )
+      ~resolve:(fun _ctx x -> x.name)
   ; field "description" ~doc:"Toy description." ~args:[] ~typ:string
-      ~resolve:(fun _ctx x -> x.description |> Util.option_of_string )
+      ~resolve:(fun _ctx x -> x.description |> Util.option_of_string)
   ; field "type" ~doc:"Toy type. Possible values are: car, animal, train."
       ~args:[] ~typ:(non_null toy_type_enum) ~resolve:(fun _ctx x ->
         x.toy_type )
   ; field "createdAt" ~doc:"Date created." ~args:[]
-      ~typ:(non_null Scalar.date_time) ~resolve:(fun _ctx x -> x.created_at
-    ) ]
+      ~typ:(non_null Scalar.date_time) ~resolve:(fun _ctx x -> x.created_at)
+  ]

test/passing/wrap_comments_break.ml
@@ -1,0 +1,8 @@
+let _ =
+  let _ =
+    fffffffffff
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      bbbbbbbbbbbbbbbbbbbbbbbbbb
+      ~f:(fun x -> return xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx)
+  in
+  2

test/passing/wrap_comments_break.ml.opts
@@ -1,0 +1,1 @@
+--no-wrap-fun-args -m 67

commit 5e498534897ac70f477b2a31526c67863bb03c4d

Fix incorrect ocaml code (#698)
* Fix bug introduced by #690 (better fix for #682)

src/Conf.ml
@@ -1666,8 +1666,7 @@ let read_config_file conf filename_kind =
                       ("unknown option", Sexp.Atom name)
                   | `Bad_value (name, reason) ->
                       ( "bad value for"
-                      , Sexp.List [Sexp.Atom name; Sexp.Atom reason] ) ))
-      )
+                      , Sexp.List [Sexp.Atom name; Sexp.Atom reason] ) )) )
     with Sys_error _ -> conf )
 
 let update_using_env conf =
@@ -1808,7 +1807,8 @@ let action =
   if !inplace then
     Inplace
       (List.map !inputs ~f:(fun file ->
-           {kind= kind_of file; name= file; file; conf= build_config ~file} ))
+           {kind= kind_of file; name= file; file; conf= build_config ~file}
+       ))
   else if !check then
     Check
       (List.map !inputs ~f:(fun file ->

src/Fmt_ast.ml
@@ -1505,12 +1505,8 @@ and fmt_expression c ?(box = true) ?pro ?epi ?eol ?parens ?(indent_wrap = 0)
                    $ fmt_or_k c.conf.indicate_multiline_delimiters
                        (fmt_or_k
                           (Location.is_single_line pexp_loc c.conf.margin)
-                          (fmt ")")
-                          (fmt_or_k
-                             (Location.is_single_line xbody.ast.pexp_loc
-                                c.conf.margin)
-                             (fmt " )") (fits_breaks "@ )" "@ )")))
-                       (fmt ")") )
+                          (fmt ")") (fits_breaks ")" "@ )"))
+                       (fits_breaks ")" "@,)") )
                $ fmt_atrs ))
       | ( lbl
         , ( { pexp_desc= Pexp_function [{pc_lhs; pc_guard= None; pc_rhs}]
@@ -2016,8 +2012,7 @@ and fmt_expression c ?(box = true) ?pro ?epi ?eol ?parens ?(indent_wrap = 0)
         ( wrap_record c
             ( opt default (fun d ->
                   hvbox 2
-                    (fmt_expression c (sub_exp ~ctx d) $ fmt "@;<1 -2>")
-              )
+                    (fmt_expression c (sub_exp ~ctx d) $ fmt "@;<1 -2>") )
             $ fmt_if (Option.is_some default) "with@;<1 2>"
             $ list flds (semic_sep c) fmt_field )
         $ fmt_atrs )
@@ -2232,8 +2227,8 @@ and fmt_class_structure c ~ctx ?ext self_ fields =
         ( fmt "object"
         $ fmt_extension_suffix c ext
         $ opt self_ (fun self_ ->
-              fmt "@;" $ wrap "(" ")" (fmt_pattern c (sub_pat ~ctx self_)) )
-        )
+              fmt "@;" $ wrap "(" ")" (fmt_pattern c (sub_pat ~ctx self_))
+          ) )
     $ cmts_after_self
     $ ( match fields with
       | ({pcf_desc= Pcf_attribute a}, _) :: _
@@ -2796,8 +2791,7 @@ and fmt_type_declaration c ?(pre = "") ?(suf = ("" : _ format)) ?(brk = suf)
                         | `Sparse -> "@;<1000 0>; "
                         | `Compact -> "@,; " )
                       $ fmt_label_declaration c ctx x ~last
-                      $ fmt_if (last && exposed_right_typ x.pld_type) " "
-                  )))
+                      $ fmt_if (last && exposed_right_typ x.pld_type) " " )))
       | `After ->
           box_manifest
             ( fmt_manifest ~priv:Public mfst

src/Translation_unit.ml
@@ -285,7 +285,8 @@ let print_error ?(quiet_unstable = false) ?(quiet_comments = false)
           | _ -> () ) ;
           if Conf.debug then
             List.iter l ~f:(fun (msg, sexp) ->
-                Format.fprintf fmt "  %s: %s\n%!" msg (Sexp.to_string sexp) )
+                Format.fprintf fmt "  %s: %s\n%!" msg (Sexp.to_string sexp)
+            )
       | exn ->
           Format.fprintf fmt
             "  BUG: unhandled exception. Use [--debug] for details.\n%!" ;
@@ -355,7 +356,8 @@ let format xunit (conf : Conf.t) ?output_file ~input_name ~source ~parsed ()
                 ; ("old ast", old_ast)
                 ; ("new ast", new_ast) ]
                 |> List.filter_map ~f:(fun (s, f_opt) ->
-                       Option.map f_opt ~f:(fun f -> (s, String.sexp_of_t f)) )
+                       Option.map f_opt ~f:(fun f -> (s, String.sexp_of_t f))
+                   )
               in
               internal_error (`Doc_comment docstrings) args
             else
@@ -365,7 +367,8 @@ let format xunit (conf : Conf.t) ?output_file ~input_name ~source ~parsed ()
                 ; ("old ast", old_ast)
                 ; ("new ast", new_ast) ]
                 |> List.filter_map ~f:(fun (s, f_opt) ->
-                       Option.map f_opt ~f:(fun f -> (s, String.sexp_of_t f)) )
+                       Option.map f_opt ~f:(fun f -> (s, String.sexp_of_t f))
+                   )
               in
               internal_error `Ast_changed args ) ;
           (* Comments not preserved ? *)

test/passing/apply.ml
@@ -24,3 +24,7 @@ let _ = ( !!! ) ~b
 let _ = !!!!a b d
 
 let _ = ( + ) a b c d
+
+let cartesian_product l1 l2 =
+  List.concat
+    (l1 |> List.map (fun v1 -> l2 |> List.map (fun v2 -> (v1, v2))))