e20f5a8ced2a0a07d8c7a89b560c0fecf5108382
hhugo <hugo.heuzard@gmail.com>
2019-04-15 09:07:33 +0800

    Provide an mli for Compat (#772)

--- src/compat.mli
+++ src/compat.mli
@@ -9,20 +9,7 @@
  *                                                                    *
  **********************************************************************)
 
-let () = Clflags.error_style := Some Misc.Error_style.Short
+val with_warning_filter :
+  filter:(Location.t -> Warnings.t -> bool) -> f:(unit -> 'a) -> 'a
 
-let with_warning_filter ~filter ~f =
-  let warning_reporter = !Location.warning_reporter in
-  (Location.warning_reporter :=
-     fun loc warn ->
-       if filter loc warn then Location.default_warning_reporter loc warn
-       else None) ;
-  let reset () = Location.warning_reporter := warning_reporter in
-  try
-    let x = f () in
-    reset () ; x
-  with e -> reset () ; raise e
-
-let print_warning l w =
-  Location.default_warning_reporter l w
-  |> Option.iter ~f:(Location.print_report Caml.Format.err_formatter)
+val print_warning : Location.t -> Warnings.t -> unit
