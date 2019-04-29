71a7356859322f319d9581781b7223be7b4fb9d4
Guillaume Petiot <guillpetiot@gmail.com>
2019-04-29 21:46:18 +0700

    colon instead of comma anyway

--- src/Fmt_ast.ml
+++ src/Fmt_ast.ml
@@ -3674,9 +3674,8 @@ and fmt_value_binding c ~rec_flag ~first ?ext ?in_ ?epi ctx binding =
     else
       match Sugar.polynewtype c.cmts pat body with
       | Some (xpat, pvars, xtyp, xbody) ->
-          let comma = fmt_or c.conf.ocp_indent_compat "@ : " " :@ " in
           let fmt_cstr =
-            comma
+            fmt_or c.conf.ocp_indent_compat "@ : " " :@ "
             $ hvbox 0
                 ( fmt "type "
                 $ list pvars " " (fmt_str_loc c)
