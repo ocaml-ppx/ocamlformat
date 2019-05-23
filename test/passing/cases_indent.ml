7ff97598fdb202f2e1fdbb6b1181187fb9dd1de8
Guillaume Petiot <guillpetiot@gmail.com>
2019-05-23 16:29:22 +0700

    add test

---
+++ test/passing/cases_indent.ml
@@ -0,0 +1,22 @@
+[@@@ocamlformat "cases-indent=4"]
+
+let foooooooo =
+  match fooooooooooooooooooooooo with
+      | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo
+
+let foooooooo =
+  try fooooooooooooooooooooooo with
+      | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo
+      | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo
+
+let foooooooo = function
+    | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo
+
+let foooooooo = function
+    | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo
+    | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo
+
+let foo =
+  fooooooooo foooooooo ~foooooooo:(function
+      | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo
+      | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo)
