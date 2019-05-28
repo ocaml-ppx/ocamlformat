2332fe9cb5eacebf555ecbcda175f7f849f8cf9c
Guillaume Petiot <guillpetiot@gmail.com>
2019-05-28 15:29:42 +0700

    more tests

--- test/passing/function_indent_always.ml
+++ test/passing/function_indent_always.ml
@@ -9,3 +9,13 @@ let foo =
   fooooooooo foooooooo ~foooooooo:(function
       | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo
       | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo)
+
+let foooooooo =
+  if fooooooooooo then
+    function
+    | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo
+    | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo
+  else
+    function
+    | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo
+    | fooooooooooooooooooooooo -> foooooooooooooooooooooooooo
