868d6b7800d2fa116aaa13e4903d361a270c31de
Jules Aguillon <juloo.dsi@gmail.com>
2019-06-04 16:58:17 +0200

    Merge let_open and open tests, open.ml.ref

--- test/passing/open.ml
+++ test/passing/open.ml
@@ -176,3 +176,12 @@ class a =
   let open (* A' *) A (* A *) in
   (* B *)
   b
+
+let _ =
+  (* a *) let (* b *) open (* c *) struct type t end (* d *) in (* e *)
+  (* f *) let (* g *) open (* h *) A (* i *) (B) (* j *) in (* k *)
+  ()
+
+(* l *) open (* m *) struct
+  type t
+end (* n *)
