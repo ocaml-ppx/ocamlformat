a3fa8aec3bccbff9e1d348743c5766b3a0e634f6
Thomas Refis <thomas.refis@gmail.com>
2019-06-08 01:20:29 +0100

    Test: extend max_indent test (#878)

--- test/passing/max_indent.ml
+++ test/passing/max_indent.ml
@@ -3,3 +3,12 @@ let () =
   |> List.iter (fun x ->
     let x = x $ y in
     fooooooooooo x)
+
+let () =
+  fooooo
+  |> List.iter
+    (fun some_really_really_really_long_name_that_doesn't_fit_on_the_line ->
+      let x =
+        some_really_really_really_long_name_that_doesn't_fit_on_the_line $ y
+      in
+      fooooooooooo x)
