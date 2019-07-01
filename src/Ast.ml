d0b06f0b7350825312fdd15b058b3d815a5a0010
Guillaume Petiot <guillpetiot@gmail.com>
2019-07-01 15:25:41 +0700

    Fix docstrings of exceptions

--- src/Ast.ml
+++ src/Ast.ml
@@ -396,7 +396,6 @@ module Signature_item : Module_item with type t = signature_item = struct
      |Psig_type (_, {ptype_attributes= atrs; _} :: _)
      |Psig_typesubst ({ptype_attributes= atrs; _} :: _)
      |Psig_typext {ptyext_attributes= atrs; _}
-     |Psig_exception {ptyexn_attributes= atrs; _}
      |Psig_modtype {pmtd_attributes= atrs; _}
      |Psig_modsubst {pms_attributes= atrs; _}
      |Psig_open {popen_attributes= atrs; _}
@@ -409,6 +408,10 @@ module Signature_item : Module_item with type t = signature_item = struct
         :: _)
      |Psig_include
         {pincl_mod= {pmty_attributes= atrs1; _}; pincl_attributes= atrs2; _}
+     |Psig_exception
+        { ptyexn_attributes= atrs1
+        ; ptyexn_constructor= {pext_attributes= atrs2; _}
+        ; _ }
      |Psig_module
         {pmd_attributes= atrs1; pmd_type= {pmty_attributes= atrs2; _}; _} ->
         Option.is_some (fst (doc_atrs (List.append atrs1 atrs2)))
