e979545f5e02cef5058435888ec56b14b2ecadbc
Guillaume Petiot <guillpetiot@gmail.com>
2019-07-02 17:28:32 +0700

    more tests

--- test/passing/monadic_binding.ml
+++ test/passing/monadic_binding.ml
@@ -5,3 +5,11 @@ let ( and* ) t1 t2 = foooooo
 let map f t =
   let* a = t in
   pure (f a)
+
+let ( and+ ) t1 t2 = ( and* ) t1 t2
+
+let ( and+ ) t1 t2 = ( and* ) t1 t2 x
+
+let ( and+ ) t1 t2 =
+  ( and* ) t1 t2 x foooooooooooooooooo foooooooooooooooooooo
+    foooooooooooooooooo foooooooooooooooooo
