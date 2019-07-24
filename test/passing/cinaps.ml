676eed8ae66b4a2d61e92d8dda681f65dfb6a953
Guillaume Petiot <guillpetiot@gmail.com>
2019-07-24 17:53:45 +0700

    add tests

---
+++ test/passing/cinaps.ml
@@ -0,0 +1,23 @@
+(*$
+  for i = 1 to 3 do
+    Printf.printf "let x%d = %d\n" i i
+  done
+*)
+let x1 = 1
+
+let x2 = 2
+
+let x3 = 3
+
+(*$*)
+
+let x = 1
+
+(*$
+  print_newline () ;
+  List.iter
+    (fun s -> Printf.printf "let ( %s ) = Pervasives.( %s )\n" s s)
+    ["+"; "-"; "*"; "/"]
+*)
+(*$*)
+let y = 2
