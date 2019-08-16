66d3901a0807939446b2705b74f4d740b34690b0
Guillaume Petiot <guillpetiot@gmail.com>
2019-08-16 13:04:22 +0700

    more tests from cinaps

--- test/passing/doc_comments.mli
+++ test/passing/doc_comments.mli
@@ -351,3 +351,31 @@ end
     [ A lot of    spaces ]
 
     [Very looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong] *)
+
+(** {[
+  for i = 1 to 3 do
+    Printf.printf "let x%d = %d\n" i i
+  done
+]} *)
+
+(** {[
+  print_newline () ;
+  List.iter
+    (fun s -> Printf.printf "let ( %s ) = Pervasives.( %s )\n" s s)
+    ["+"; "-"; "*"; "/"]
+]}  *)
+
+(** {[
+  ;; #use "import.cinaps"
+
+  ;; List.iter all_fields ~f:(fun (name, type_) -> printf "\nexternal get_%s
+  : unit -> %s = \"get_%s\"" name type_ name)
+]} *)
+
+(** {[
+  let x = 1 in
+  (* fooooooo *)
+  let y = 2 in
+  (* foooooooo *)
+  z
+]} *)
