62264b0011a45d929c71de6edf1a1e47728bf228
Guillaume Petiot <guillaume@tarides.com>
2019-08-27 20:50:34 +0700

    Fix parens around constrained expr with attrs (#987)

--- test/passing/attribute_and_expression.ml
+++ test/passing/attribute_and_expression.ml
@@ -5,3 +5,9 @@ let tail1 = ([1; 2] [@hello])
 let tail2 = 0 :: ([1; 2] [@hello])
 
 let tail3 = 0 :: ([] [@hello])
+
+let _ = ("%d" : _ format) [@p]
+
+let _ = (`B `N : p2) [@p]
+
+let _ = (`So (`Se (`So `O)) : podd) [@p]
