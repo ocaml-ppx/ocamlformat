61f57712995a881d25ea5ceb3437488059c50755
Guillaume Petiot <guillpetiot@gmail.com>
2019-08-30 14:53:25 +0700

    fix space left

--- test/passing/max_indent.ml
+++ test/passing/max_indent.ml
@@ -7,16 +7,29 @@ let () =
 let () =
   fooooo
   |> List.iter
-    (fun some_really_really_really_long_name_that_doesn't_fit_on_the_line ->
-      let x =
-        some_really_really_really_long_name_that_doesn't_fit_on_the_line $ y
-      in
-      fooooooooooo x)
+    (fun some_really_really_really_long_name_that_doesn't_fit_on_the_line
+      ->
+        let x =
+          some_really_really_really_long_name_that_doesn't_fit_on_the_line
+          $ y
+        in
+        fooooooooooo x)
 
 let foooooooooo =
   foooooooooooooooooooooo
   |> Option.bind ~f:(function
     | Pform.Expansion.Var (Values l) -> Some (static l)
     | Macro (Ocaml_config, s) ->
-      Some (static (expand_ocaml_config (Lazy.force ocaml_config) var s))
+      Some
+        (static (expand_ocaml_config (Lazy.force ocaml_config) var s))
     | Macro (Env, s) -> Option.map ~f:static (expand_env t var s))
+
+let fooooooooooooo =
+  match lbls with
+  | (_, {lbl_all}, _) :: _ ->
+    let t =
+      Array.map
+        (fun lbl -> (mknoloc (Longident.Lident "?temp?"), lbl, omega))
+        lbl_all
+    in
+    fooooooo
