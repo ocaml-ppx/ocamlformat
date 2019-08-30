fd02499c2448ed199c55680db43278711a4cbbcd
Guillaume Petiot <guillpetiot@gmail.com>
2019-08-30 12:59:38 +0700

    Fix max-indent by memorizing suggested an real indents

--- test/passing/max_indent.ml
+++ test/passing/max_indent.ml
@@ -12,3 +12,11 @@ let () =
         some_really_really_really_long_name_that_doesn't_fit_on_the_line $ y
       in
       fooooooooooo x)
+
+let foooooooooo =
+  foooooooooooooooooooooo
+  |> Option.bind ~f:(function
+    | Pform.Expansion.Var (Values l) -> Some (static l)
+    | Macro (Ocaml_config, s) ->
+      Some (static (expand_ocaml_config (Lazy.force ocaml_config) var s))
+    | Macro (Env, s) -> Option.map ~f:static (expand_env t var s))
