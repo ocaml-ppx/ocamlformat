59ca7c2af868b0059b60608609fb931b1e313f2e
Jules Aguillon <juloo.dsi@gmail.com>
2019-09-24 12:22:14 +0200

    Improve: remove the bisect_ppx dependency and clean Makefile (#1005)
    * Cleanup Makefile
    
    Remove `dune-workspace` generation
    Don't use paths to `_build`
    Remove bisect support
    Remove plugin code from dune file
    
    * Cleanup .gitignore
    
    * Don't build whole package
    
    * test-extra: Remove unused code
    
    * test_branch: Use temporary worktree
    
    * Add -noassert in release mode
    
    * Remove dependency to bisect_ppx
    
    * Add placeholder for bisect ppx
    
    * Add tools/bisect.sh

--- tools/test_branch.sh
+++ tools/test_branch.sh
@@ -31,9 +31,17 @@ fi
 
 base=$(git merge-base master $branch)
 
-git checkout $base
-make
-OCAMLFORMAT="$2" make -C test-extra test_setup test_unstage test_clean test_pull test test_stage
-git checkout $branch
-make
-OCAMLFORMAT="$3" make -C test-extra test test_diff
+# First arg is git rev others are passed to make -C test-extra
+run_in_worktree ()
+{
+  local tmp=`mktemp -d`
+  git worktree add --detach "$tmp" "$1"
+  shift
+  make -C "$tmp"
+  local exe=`ls "$tmp"/_build/{default,dev}/src/ocamlformat.exe 2>/dev/null | head -n1`
+  OCAMLFORMAT_EXE=$exe make -C test-extra "$@"
+  git worktree remove -f "$tmp"
+}
+
+OCAMLFORMAT="$2" run_in_worktree "$base" test_setup test_unstage test_clean test_pull test test_stage
+OCAMLFORMAT="$3" run_in_worktree "$branch" test test_diff
