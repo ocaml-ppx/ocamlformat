769b00632dbe8412151986e44e61b244db5b5636
Guillaume Petiot <guillaume@tarides.com>
2019-10-10 12:29:46 +0700

    CI: use a script travis-ci.sh to simplify .travis.yml (#1063)

---
+++ tools/travis-ci.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+######################################################################
+#                                                                    #
+#                            OCamlFormat                             #
+#                                                                    #
+#  Copyright (c) 2019-present, Facebook, Inc.  All rights reserved.  #
+#                                                                    #
+#  This source code is licensed under the MIT license found in the   #
+#  LICENSE file in the root directory of this source tree.           #
+#                                                                    #
+######################################################################
+
+# Warning: this script should only be called from .travis.yml
+
+set -e
+
+CheckBuild () {
+    # before_install
+    wget -O ${HOME}/opam https://github.com/ocaml/opam/releases/download/2.0.5/opam-2.0.5-x86_64-linux
+    chmod +x ${HOME}/opam
+    export PATH=${HOME}:${PATH}
+    export OPAMYES=1
+    export OPAMJOBS=2
+    opam init --bare --disable-sandboxing
+    opam switch --dry-run ${OCAML_VERSION} 2>/dev/null || opam switch create ${OCAML_VERSION}
+    # install
+    opam switch ${OCAML_VERSION}
+    opam update --upgrade
+    opam pin add --no-action ocamlformat .
+    opam install --deps-only ocamlformat
+    # script
+    opam exec -- make
+    opam install ocamlformat
+}
+
+CheckBuildAndTests () {
+    # before_install
+    wget -O ${HOME}/opam https://github.com/ocaml/opam/releases/download/2.0.5/opam-2.0.5-x86_64-linux
+    chmod +x ${HOME}/opam
+    export PATH=${HOME}:${PATH}
+    export OPAMYES=1
+    export OPAMJOBS=2
+    opam init --bare --disable-sandboxing
+    opam switch --dry-run ${OCAML_VERSION} 2>/dev/null || opam switch create ${OCAML_VERSION}
+    # install
+    opam switch ${OCAML_VERSION}
+    opam update --upgrade
+    opam pin add --no-action ocamlformat .
+    opam install --deps-only ocamlformat
+    # script
+    opam exec -- make test
+    opam install ocamlformat
+}
+
+CheckChangesModified () {
+    if [ "$TRAVIS_PULL_REQUEST" != false ] && [ "$TRAVIS_BRANCH" = master ]
+    then
+        git diff --exit-code "$TRAVIS_BRANCH...$TRAVIS_COMMIT" -- CHANGES.md \
+            > /dev/null && exit 1 || echo pass
+    fi
+}
+
+case $CI_KIND in
+build)
+    CheckBuild
+    ;;
+build-and-tests)
+    CheckBuildAndTests
+    ;;
+changes)
+    CheckChangesModified
+    ;;
+*)
+    echo unknown CI kind
+    exit 1
+    ;;
+esac
