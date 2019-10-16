9ead276ef0a57638918a824acd69b8f54c935436
Guillaume Petiot <guillaume@tarides.com>
2019-10-16 22:28:02 +0700

    CI: factorize tests and check reason build (#1079)
    * CI: factorize tests and check reason build
    * Remove unecessary opam install

--- tools/travis-ci.sh
+++ tools/travis-ci.sh
@@ -31,26 +31,13 @@ CheckBuild () {
     opam install --deps-only ocamlformat
     # script
     opam exec -- make
-    opam install ocamlformat
+    opam exec -- make reason
 }
 
-CheckBuildAndTests () {
-    # before_install
-    wget -O ${HOME}/opam https://github.com/ocaml/opam/releases/download/2.0.5/opam-2.0.5-x86_64-linux
-    chmod +x ${HOME}/opam
-    export PATH=${HOME}:${PATH}
-    export OPAMYES=1
-    export OPAMJOBS=2
-    opam init --bare --disable-sandboxing
-    opam switch --dry-run ${OCAML_VERSION} 2>/dev/null || opam switch create ${OCAML_VERSION}
-    # install
-    opam switch ${OCAML_VERSION}
-    opam update --upgrade
-    opam pin add --no-action ocamlformat .
-    opam install --deps-only ocamlformat
+CheckTests () {
     # script
     opam exec -- make test
-    opam install ocamlformat
+    # do not run 'make test-reason' because of heavy deps
 }
 
 CheckChangesModified () {
@@ -66,7 +53,8 @@ build)
     CheckBuild
     ;;
 build-and-tests)
-    CheckBuildAndTests
+    CheckBuild
+    CheckTests
     ;;
 changes)
     CheckChangesModified
