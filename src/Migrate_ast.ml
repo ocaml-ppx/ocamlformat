c949bef04771e9765e3ab775a57809bdc3b3834e
Josh Berdine <jjb@fb.com>
2019-10-26 15:46:24 +0100

    Adjust definition of Location.is_single_line to reflect margin
    This seems to be needed to be consistent with recent changes to the
    handling of the margin. Otherwise, it is possible to have a one-liner
    structure item of just the right width that is_single_line does not
    hold, resulting in the one-liner being surrounded by open lines.

--- src/Migrate_ast.ml
+++ src/Migrate_ast.ml
@@ -159,7 +159,7 @@ module Location = struct
     | n -> n
 
   let is_single_line x margin =
-    width x <= margin && x.loc_start.pos_lnum = x.loc_end.pos_lnum
+    width x <= margin + 1 && x.loc_start.pos_lnum = x.loc_end.pos_lnum
 
   let smallest loc stack =
     let min a b = if width a < width b then a else b in
