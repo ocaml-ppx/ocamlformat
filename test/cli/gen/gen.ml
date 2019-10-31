e08a11f9078f60c148fee88e71a817328bae1dc8
Jules Aguillon <juloo.dsi@gmail.com>
2019-10-31 18:01:23 +0100

    Show error when .opts or .ref file is missing

--- test/cli/gen/gen.ml
+++ test/cli/gen/gen.ml
@@ -1,10 +1,14 @@
 module StringMap = Map.Make (String)
 
 type entry = {
+  has_ref : bool;
+  has_opts : bool;
   should_fail : bool
 }
 
 let empty_entry = {
+  has_ref = false;
+  has_opts = false;
   should_fail = false;
 }
 
@@ -15,12 +19,23 @@ let register_file entries fname =
         | None -> Some (f empty_entry)) entries
   in
   match String.split_on_char '.' fname with
+  | [ test_name; "ref" ] ->
+      update_or_add test_name ~f:(fun e -> { e with has_ref = true })
   | [ test_name; "opts" ] ->
-      update_or_add test_name ~f:(fun e -> e)
+      update_or_add test_name ~f:(fun e -> { e with has_opts = true })
   | [ test_name; "should-fail" ] ->
-      update_or_add test_name ~f:(fun _e -> { should_fail = true })
+      update_or_add test_name ~f:(fun e -> { e with should_fail = true })
   | _ ->
       entries
+
+let check_test test_name entry ok =
+  let e ok b =
+    if b then Format.kfprintf (fun _ -> false) Format.err_formatter
+    else Format.ikfprintf (fun _ -> ok) Format.err_formatter
+  in
+  let ok = e ok (not entry.has_ref) "@{<error>Error@}: Missing file %s.ref\n" test_name in
+  let ok = e ok (not entry.has_opts) "@{<error>Error@}: Missing file %s.opts\n" test_name in
+  ok
 
 let emit_test test_name entry =
   let cmd_prefix = if entry.should_fail then "! " else "" in
@@ -39,6 +54,8 @@ let emit_test test_name entry =
     test_name test_name
 
 let () =
-  Sys.readdir "."
-  |> Array.fold_left register_file StringMap.empty
-  |> StringMap.iter emit_test
+  let files = Sys.readdir "." in
+  let tests = Array.fold_left register_file StringMap.empty files in
+  if not (StringMap.fold check_test tests true) then
+    exit 1;
+  StringMap.iter emit_test tests
