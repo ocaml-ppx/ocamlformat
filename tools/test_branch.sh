e4dfacc54cc145918e8ded05991ddcae34211d71
Jules Aguillon <juloo.dsi@gmail.com>
2020-01-23 15:59:18 +0100

    Revert to the old Makefile, remove crazy code

--- tools/test_branch.sh
+++ tools/test_branch.sh
@@ -27,75 +27,6 @@
 
 set -e
 
-CODE_CACHE=/tmp/ocamlformat_test_branch_cache
-
-# Arguments: name git_uri [excluded_dirs...]
-# Env: DST_DIR
-fetch_project ()
-{
-  local name="$1" git_uri="$2"
-  shift 2
-  local code="$CODE_CACHE/$name"
-  if ! [[ -d $code ]]; then
-    mkdir -p "$code"
-    git clone "$git_uri" "$code"
-  fi
-
-  local f dst="$DST_DIR/$name" exclude_args=()
-  for f in ./_build "$@"; do exclude_args+=(-path "./$f" -not -prune -or); done
-
-  ( cd "$code"
-    find . "${exclude_args[@]}" \
-      -name '*.ml' -and -not -name '*.pp.ml' -or \
-      -name '*.mli' -and -not -name '*.pp.mli' -or \
-      -name '*.opam' -or -name '.ocamlformat*'
-  ) |
-  while read f; do
-    mkdir -p "$dst/${f%/*}"
-    cp "$code/$f" "$dst/$f"
-  done
-}
-
-# Copy files to be formatted for every projects in $DST_DIR/project_name
-# Env: DST_DIR
-fetch_projects ()
-{
-  fetch_project ocamlformat "`pwd`" test
-  fetch_project infer "https://github.com/facebook/infer.git"
-  fetch_project js_of_ocaml "https://github.com/ocsigen/js_of_ocaml.git"
-  fetch_project ocaml "https://github.com/ocaml/ocaml.git" experimental testsuite/tests
-  fetch_project dune "https://github.com/ocaml/dune.git" test vendor otherlibs example
-  # fetch_project owl "https://github.com/owlbarn/owl"
-  # fetch_project irmin "https://github.com/mirage/irmin"
-  # fetch_project index "https://github.com/mirage/index"
-  # fetch_project duniverse "https://github.com/ocamllabs/duniverse" duniverse
-  # fetch_project dune-release "https://github.com/ocamllabs/dune-release"
-}
-
-# Arguments: ocamlformat_rev src_dir dst_dir
-# Env: OCAMLFORMAT
-run_in_worktree ()
-{
-  local rev="$1" src_dir="$2" dst_dir="$3"
-  local tmp=`mktemp -d`
-  echo "Building $rev in $tmp"
-  git worktree add --detach "$tmp" "$rev"
-  shift
-  ( cd "$tmp"
-    dune build -p ocamlformat
-    dune install --prefix=dist ocamlformat &>/dev/null )
-  local exe="$tmp/dist/bin/ocamlformat"
-  echo "Built $exe"
-
-  ( cd "$src_dir"
-    find . -type d -exec mkdir -p "$dst_dir/{}" \;
-    find . -type f -name '*.ml' -or -name '*.mli' |
-    parallel --bar "$exe" --enable-outside-detected-project --no-version-check -o "$dst_dir/{}" "{}"
-  ) || true
-
-  git worktree remove --force "$tmp"
-}
-
 opt_a=
 opt_b=
 while getopts "a:b:" opt; do
@@ -117,20 +48,24 @@ if [[ "$rev_a" = "$rev_b" ]]; then
   exit 1
 fi
 
-base_dir=`mktemp -d`
-src_dir="$base_dir/src"
-a_dir="$base_dir/a"
-b_dir="$base_dir/b"
+# First arg is git rev others are passed to make -C test-extra
+# Env: OCAMLFORMAT
+run_in_worktree ()
+{
+  local tmp=`mktemp -d`
+  echo "Building $1 in $tmp"
+  git worktree add --detach "$tmp" "$1"
+  shift
+  ( cd "$tmp"
+    dune build -p ocamlformat
+    dune install --prefix=dist ocamlformat &>/dev/null )
+  local exe="$tmp/dist/bin/ocamlformat"
+  echo "Built $exe"
+  make -C test-extra "OCAMLFORMAT_EXE=$exe" "$@"
+  git worktree remove --force "$tmp"
+}
 
-DST_DIR=$src_dir fetch_projects
+make -C test-extra test_setup test_unstage test_clean test_pull
 
-OCAMLFORMAT="$opts_a" run_in_worktree "$rev_a" "$src_dir" "$a_dir"
-OCAMLFORMAT="$opts_b" run_in_worktree "$rev_b" "$src_dir" "$b_dir"
-
-for p in `ls "$src_dir"`; do
-  report="test_branch_${p}_${rev_a:0:8}_${rev_b:0:8}.diff"
-  git diff --no-index "$a_dir/$p" "$b_dir/$p" > "$report" || true
-  echo "Wrote diffs to $report"
-done
-
-rm -r "$base_dir"
+OCAMLFORMAT="$opts_a" run_in_worktree "$rev_a" test test_stage
+OCAMLFORMAT="$opts_b" run_in_worktree "$rev_b" test test_diff
