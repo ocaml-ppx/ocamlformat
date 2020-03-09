1a35c230fb658b765b2a385861dfca61dc13f7da
Guillaume Petiot <guillpetiot@gmail.com>
2020-03-09 19:01:14 +0700

    Add test

--- test/passing/index_op.ml
+++ test/passing/index_op.ml
@@ -120,3 +120,12 @@ class free =
   object (m : 'test)
     method get_def = m#state.def
   end
+
+let ( .%{;..} ) = Genarray.get
+
+let ( .%{;..}<- ) = Genarray.set
+
+let () =
+  let x = Genarray.create Float64 c_layout [|3; 4; 5|] in
+  x.%{0; 0; 0} <- 3. ;
+  Printf.printf "%f\n" x.%{0; 0; 0}
