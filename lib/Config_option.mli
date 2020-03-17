37c157167803d1a24f95c7ffd5cee0789a511803
juloo <juloo.dsi@gmail.com>
2020-03-17 19:09:35 +0100

    Declare removed options
    Using them result in a helpful error.
    A "REMOVED OPTIONS" section is added to the man page.

--- lib/Config_option.mli
+++ lib/Config_option.mli
@@ -62,6 +62,11 @@ module Make (C : CONFIG) : sig
   val any :
     'a Cmdliner.Arg.conv -> default:'a -> docv:string -> 'a option_decl
 
+  val removed_option :
+    names:string list -> version:string -> msg:string -> unit
+  (** Declare an option as removed. Using such an option will result in an
+      helpful error including [msg] and [version]. *)
+
   val default : 'a t -> 'a
 
   val update_using_cmdline : config -> config
