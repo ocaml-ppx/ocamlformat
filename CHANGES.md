3e48f7e45930bca1a88a1a240a06a9ef5a816eba
Guillaume Petiot <guillaume@tarides.com>
2020-06-04 14:17:47 +0200

    Distinguish hash-getter from hash-comparison infix operators (#1376)

--- CHANGES.md
+++ CHANGES.md
@@ -4,6 +4,8 @@
 
   + Do not break inline elements such as `{i blah}` in docstrings (#1346) (Josh Berdine)
 
+  + Distinguish hash-getter from hash-comparison infix operators. Operators of the form `#**#` or `#**.` where `**` can be 0 or more operator chars are considered getter operators and are not surrounded by spaces, as opposed to regular infix operators (#1376) (Guillaume Petiot)
+
 #### Bug fixes
 
   + Restore previous functionality for pre-post extension points (#1342) (Josh Berdine)
--- lib/Ast.ml
+++ lib/Ast.ml
@@ -61,20 +61,30 @@ let is_monadic_binding e =
   | Pexp_ident {txt= Lident i; _} -> is_monadic_binding_id i
   | _ -> false
 
-let is_infix_id i =
-  match (i.[0], i) with
-  | ( ( '$' | '%' | '*' | '+' | '-' | '/' | '<' | '=' | '>' | '|' | '&' | '@'
-      | '^' | '#' )
-    , _ )
-   |( _
-    , ( "!=" | "land" | "lor" | "lxor" | "mod" | "::" | ":=" | "asr" | "lsl"
-      | "lsr" | "or" | "||" ) ) ->
+let is_infixopchar = function
+  | '$' | '%' | '*' | '+' | '-' | '/' | '<' | '=' | '>' | '|' | '&' | '@'
+   |'^' | '#' ->
       true
-  | _ -> is_monadic_binding_id i
+  | _ -> false
+
+let is_infix_id i =
+  if is_infixopchar i.[0] then true
+  else
+    match i with
+    | "!=" | "land" | "lor" | "lxor" | "mod" | "::" | ":=" | "asr" | "lsl"
+     |"lsr" | "or" | "||" ->
+        true
+    | _ -> is_monadic_binding_id i
 
 let is_infix e =
   match e.pexp_desc with
   | Pexp_ident {txt= Lident i; _} -> is_infix_id i
+  | _ -> false
+
+let is_hash_getter_id i =
+  let is_infix_char c = Char.equal c '.' || is_infixopchar c in
+  match (i.[0], i.[String.length i - 1]) with
+  | '#', ('#' | '.') when String.for_all i ~f:is_infix_char -> true
   | _ -> false
 
 module Indexing_op = struct
