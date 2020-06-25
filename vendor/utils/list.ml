ee3bebfad85b01b217accb3e7ae684c86e7500bd
Guillaume Petiot <guillpetiot@gmail.com>
2020-06-25 18:42:00 +0700

    depend on Base instead

--- vendor/utils/list.ml
+++ vendor/utils/list.ml
@@ -1,12 +1,12 @@
-module L = Stdlib.List
+open Base
 
 let partition_map l ~f =
   let fst, snd =
-    L.fold_left
-      (fun (fst, snd) x ->
+    List.fold_left l
+      ~f:(fun (fst, snd) x ->
         match f x with
         | `Fst x' -> (x' :: fst, snd)
         | `Snd x' -> (fst, x' :: snd) )
-      ([], []) l
+      ~init:([], [])
   in
-  (L.rev fst, L.rev snd)
+  (List.rev fst, List.rev snd)
