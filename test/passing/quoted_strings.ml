9a25b86853ffc909244c70f90ecfcfd0bb0269f5
Guillaume Petiot <guillpetiot@gmail.com>
2020-07-01 22:14:51 +0700

    fix tests

--- test/passing/quoted_strings.ml
+++ test/passing/quoted_strings.ml
@@ -1,10 +1,9 @@
 let foo = {%foo | foooooooooooooo |}
 
 let foo =
-  (* A *)
-  ( {%foo | foooooooooooooo | (* B *) } (* C *)
+  ( {%foo | foooooooooooooo | (* B *) } (* C *))
 
-let foo = {%foo| foooooooooooooo |}
+let foo = ({%foo| foooooooooooooo |}
   [@attr] )
 
 let foo = (* A *) ({%foo | foooooooooooooo |} (* B *) [@attr]) (* C *)
@@ -14,10 +13,9 @@ let foo = (* A *) {%foo sep| foooooooooooooo |sep}
 let foo = {%foo | foooooooooooooo |} [@@attr]
 
 let foo =
-  (* A *)
-  ( {%foo | foooooooooooooo | (* B *) } (* C *) [@@attr] (* D *)
+  ( {%foo | foooooooooooooo | (* B *) } (* C *) [@@attr] (* D *))
 
-let foo = {%foo| foooooooooooooo |}
+let foo = ({%foo| foooooooooooooo |}
   [@attr] )
   [@@attr]
 
@@ -26,3 +24,16 @@ let foo = ({%foo | foooooooooooooo |} (* A *) [@attr]) (* B *) [@@attr]
 let foo = (* A *) ({%foo | foooooooooooooo |} [@attr]) (* B *) [@@attr]
 
 let foo = (* A *) {%foo sep| foooooooooooooo |sep} (* B *) [@@attr]
+
+{%%foo | foooooooooooooo |}
+
+{%%foo | foooooooooooooo | (* A *) } (* B *)
+
+;; {%%foo| foooooooooooooo |}
+[@@attr]
+
+{%%foo | foooooooooooooo |} (* A *) [@@attr]
+
+{%%foo sep| foooooooooooooo |sep}
+
+{%%foo sep| foooooooooooooo |sep} (* A *) [@@attr]
