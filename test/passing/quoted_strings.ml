f1c41352c89f4afbbbdf2acd29b9c14cc1096ea4
Guillaume Petiot <guillpetiot@gmail.com>
2020-07-01 22:27:16 +0700

    add tests from compiler

--- test/passing/quoted_strings.ml
+++ test/passing/quoted_strings.ml
@@ -37,3 +37,29 @@ let foo = (* A *) {%foo sep| foooooooooooooo |sep} (* B *) [@@attr]
 {%%foo sep| foooooooooooooo |sep}
 
 {%%foo sep| foooooooooooooo |sep} (* A *) [@@attr]
+
+(* Structures *)
+{%%M.foo | <hello>{x} |}
+
+{%%M.foo bar| <hello>{|x|} |bar}
+
+(* Signatures *)
+module type S = sig
+  {%%M.foo | <hello>{x} |}
+
+  {%%M.foo bar| <hello>{|x|} |bar}
+end
+
+(* Expressions/Pattern/Types *)
+let ({%M.foo | <hello>{x} |} : {%M.foo | <hello>{x} |}) =
+  {%M.foo | <hello>{x} |}
+
+let ({%M.foo bar| <hello>{|x|} |bar} : {%M.foo bar| <hello>{|x|} |bar}) =
+  {%M.foo bar| <hello>{|x|} |bar}
+
+(* Multiline *)
+{%%M.foo |
+ <hello>
+   {x}
+ </hello>
+|}
