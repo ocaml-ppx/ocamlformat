0fd8f63fc62dfbcc12467ad5416b3af8f6dcc14b
Etienne Millon <me@emillon.org>
2020-07-24 14:57:27 +0200

    Write on stdout

--- vendor/compat/gen/gen.ml
+++ vendor/compat/gen/gen.ml
@@ -1,11 +1,6 @@
-let write fn s =
-  let oc = open_out fn in
-  output_string oc s ; close_out oc
-
 let () =
   let ocaml_version_str = Sys.argv.(1) in
   let ocaml_version =
     Scanf.sscanf ocaml_version_str "%u.%u" (fun a b -> (a, b))
   in
-  write "warning-compat-file"
-    (if ocaml_version < (4, 08) then "lt_408.ml" else "ge_408.ml")
+  print_string (if ocaml_version < (4, 08) then "lt_408.ml" else "ge_408.ml")
