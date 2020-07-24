1cb11afc6bbd75c4d5e21ce17d8fcc66ae3d49ab
Etienne Millon <me@emillon.org>
2020-07-24 14:59:04 +0200

    Use Sys.ocaml_version directly

--- vendor/compat/gen/gen.ml
+++ vendor/compat/gen/gen.ml
@@ -1,6 +1,5 @@
 let () =
-  let ocaml_version_str = Sys.argv.(1) in
   let ocaml_version =
-    Scanf.sscanf ocaml_version_str "%u.%u" (fun a b -> (a, b))
+    Scanf.sscanf Sys.ocaml_version "%u.%u" (fun a b -> (a, b))
   in
   print_string (if ocaml_version < (4, 08) then "lt_408.ml" else "ge_408.ml")
