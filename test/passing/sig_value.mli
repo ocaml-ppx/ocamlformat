fd7fde68143812e9bda4b669bf83b4ac8f50f30e
Guillaume Petiot <guillaume@tarides.com>
2020-07-31 23:17:22 +0700

    Fix attributes position in labelled arguments type

---
+++ test/passing/sig_value.mli
@@ -0,0 +1,9 @@
+val f : f:(string[@att]) (** doc *) -> unit
+val f : f:(string[@att]) (** doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc *) -> unit
+val f : f:(string[@att]) -> unit
+val f : f:string (** doc *) -> unit
+
+val f : f:((string * string)[@att]) (** doc *) -> unit
+val f : f:((string * string)[@att]) (** doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc doc *) -> unit
+val f : f:((string * string)[@att]) -> unit
+val f : f:((string * string)) (** doc *) -> unit
