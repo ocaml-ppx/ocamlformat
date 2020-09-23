dfe2b962b06142c25dc4501b3a5714ac39324a99
Etienne Millon <me@emillon.org>
2020-09-23 17:10:27 +0200

    parse_wyc: removed unused modules from Migrate_ast (#1472)
    * parse-wyc: remove pprintast
    
    * Remove Printast
    
    * Remove Parse
    
    * Remove selected_version

--- vendor/parse-wyc/lib/migrate_ast.ml
+++ vendor/parse-wyc/lib/migrate_ast.ml
@@ -1,5 +1,3 @@
-let selected_version = Migrate_parsetree.Versions.ocaml_408
-
 module Selected_version = Migrate_parsetree.Ast_408
 module Ast_mapper = Selected_version.Ast_mapper
 module Ast_helper = Selected_version.Ast_helper
@@ -56,68 +54,6 @@ module Docstrings = struct
     lbs_extension : string Asttypes.loc option;
     lbs_loc : Location.t;
   }
-end
-
-module Parse = struct
-  open Migrate_parsetree
-
-  let implementation = Parse.implementation selected_version
-
-  let interface = Parse.interface selected_version
-
-  let use_file lexbuf =
-    List.filter
-      (fun (p : Parsetree.toplevel_phrase) ->
-        match p with
-        | Ptop_def [] -> false
-        | Ptop_def (_ :: _) | Ptop_dir _ -> true)
-      (Parse.use_file selected_version lexbuf)
-end
-
-let to_current =
-  Migrate_parsetree.Versions.(migrate selected_version ocaml_current)
-
-module Printast = struct
-  open Printast
-
-  let implementation f x = implementation f (to_current.copy_structure x)
-
-  let interface f x = interface f (to_current.copy_signature x)
-
-  let expression n f x = expression n f (to_current.copy_expression x)
-
-  let payload n f (x : Parsetree.payload) =
-    payload n f
-      ( match x with
-      | PStr x -> PStr (to_current.copy_structure x)
-      | PSig x -> PSig (to_current.copy_signature x)
-      | PTyp x -> PTyp (to_current.copy_core_type x)
-      | PPat (x, Some y) ->
-          PPat (to_current.copy_pattern x, Some (to_current.copy_expression y))
-      | PPat (x, None) -> PPat (to_current.copy_pattern x, None) )
-
-  let use_file f (x : Parsetree.toplevel_phrase list) =
-    List.iter
-      (fun (p : Parsetree.toplevel_phrase) ->
-        top_phrase f (to_current.copy_toplevel_phrase p))
-      x
-end
-
-module Pprintast = struct
-  open Pprintast
-
-  let structure f x = structure f (to_current.copy_structure x)
-
-  let signature f x = signature f (to_current.copy_signature x)
-
-  let core_type f x = core_type f (to_current.copy_core_type x)
-
-  let expression f x = expression f (to_current.copy_expression x)
-
-  let pattern f x = pattern f (to_current.copy_pattern x)
-
-  let toplevel_phrase f x =
-    toplevel_phrase f (to_current.copy_toplevel_phrase x)
 end
 
 module Int = struct
