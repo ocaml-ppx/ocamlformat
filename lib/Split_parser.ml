d4a08fbbda9b63342e98000c82e46efb46c8239d
Guillaume Petiot <guillaume@tarides.com>
2020-10-05 20:48:42 +0700

    lexing exceptions appear at the end of lexing, so let's add EOF and consider it done

--- lib/Split_parser.ml
+++ lib/Split_parser.ml
@@ -26,7 +26,8 @@ module Line = struct
     let lexbuf = Lexing.from_string str in
     let rec loop acc =
       match Lexer.token lexbuf with
-      | Parser.EOF -> List.rev acc
+      | exception _ -> List.rev (Parser.EOF :: acc)
+      | Parser.EOF -> List.rev (Parser.EOF :: acc)
       | tok -> loop (tok :: acc)
     in
     loop []
