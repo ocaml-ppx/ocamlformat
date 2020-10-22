3f8f055c436248b4634b4419993d58733a3af9cc
Etienne Millon <me@emillon.org>
2020-10-22 18:21:29 +0200

    Simpler test

--- test/unit/test_fmt.ml
+++ test/unit/test_fmt.ml
@@ -8,42 +8,39 @@ let eval_fmt term =
   Format_.pp_print_flush ppf () ;
   Buffer.contents buffer
 
-let join l = List.fold_left l ~init:Fmt.noop ~f:Fmt.( $ )
-
-let map_string s ~f = String.to_list s |> List.map ~f |> join
-
 let tests_lazy =
-  let test name term ~expected =
-    ( "lazy_: " ^ name
+  [ ( "lazy_: not using lazy"
     , `Quick
     , fun () ->
+        let r = ref None in
+        let pp s =
+          r := Some s ;
+          Fmt.str s
+        in
+        let term = Fmt.fmt_if_k false (pp "hello") in
+        let expected = "" in
+        let expected_r = Some "hello" in
         let got = eval_fmt term in
-        Alcotest.check Alcotest.(string) Caml.__LOC__ expected got )
-  in
-  let is_vowel = function
-    | 'a' | 'e' | 'i' | 'o' | 'u' | 'y' -> true
-    | _ -> false
-  in
-  [ test "not using lazy"
-      (let i = ref 0 in
-       let pp_letter c =
-         let open Fmt in
-         Int.incr i ;
-         str (Int.to_string !i) $ char c
-       in
-       let pp_if_vowel c = Fmt.fmt_if_k (is_vowel c) (pp_letter c) in
-       map_string "hello" ~f:pp_if_vowel)
-      ~expected:"2e5o"
-  ; test "using lazy"
-      (let i = ref 0 in
-       let pp_letter c =
-         let open Fmt in
-         lazy_ (fun () ->
-             Int.incr i ;
-             str (Int.to_string !i) $ char c)
-       in
-       let pp_if_vowel c = Fmt.fmt_if_k (is_vowel c) (pp_letter c) in
-       map_string "hello" ~f:pp_if_vowel)
-      ~expected:"1e2o" ]
+        let got_r = !r in
+        Alcotest.check Alcotest.(string) Caml.__LOC__ expected got ;
+        Alcotest.check Alcotest.(option string) Caml.__LOC__ expected_r got_r
+    )
+  ; ( "lazy_: using lazy"
+    , `Quick
+    , fun () ->
+        let r = ref None in
+        let pp s =
+          Fmt.lazy_ (fun () ->
+              r := Some s ;
+              Fmt.str s)
+        in
+        let term = Fmt.fmt_if_k false (pp "hello") in
+        let expected = "" in
+        let expected_r = None in
+        let got = eval_fmt term in
+        let got_r = !r in
+        Alcotest.check Alcotest.(string) Caml.__LOC__ expected got ;
+        Alcotest.check Alcotest.(option string) Caml.__LOC__ expected_r got_r
+    ) ]
 
 let tests = tests_lazy
