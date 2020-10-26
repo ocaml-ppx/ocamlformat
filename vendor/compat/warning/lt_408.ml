3318c18121c67d5ae0b02ad417426d53dffd897b
Etienne Millon <me@emillon.org>
2020-10-26 13:48:34 +0100

    Add symbolic names when printing warnings
    This is the new behavior on 4.12. For 4.08 to 4.11 it is possible to map
    the string in the report value, but for 4.06 and 4.07 we need to parse
    the string that is being printed.
    
    See #1508

--- vendor/compat/warning/lt_408.ml
+++ vendor/compat/warning/lt_408.ml
@@ -1,3 +1,5 @@
+open Base
+
 let with_warning_filter ~filter ~f =
   let warning_printer = !Location.warning_printer in
   (Location.warning_printer :=
@@ -9,8 +11,39 @@ let with_warning_filter ~filter ~f =
     reset () ; x
   with e -> reset () ; raise e
 
+let print_to_string f =
+  let buf = Buffer.create 0 in
+  let ppf = Caml.Format.formatter_of_buffer buf in
+  f ppf;
+  Caml.Format.pp_print_flush ppf ();
+  Buffer.contents buf
+
+let expand_names_on_line s =
+  let open Option in
+  let parsed_line =
+    String.index s ':' >>= fun colon_pos ->
+    let before_colon = String.subo s ~len:(colon_pos) in
+    String.chop_prefix before_colon ~prefix:"Warning " >>= fun n_str ->
+    Caml.int_of_string_opt n_str >>= fun n ->
+    Warning_name.warning_name n >>| fun name ->
+    let rest = String.subo s ~pos:colon_pos in
+    (n, name, rest)
+  in
+  match parsed_line with
+  | None -> s
+  | Some (n, name, rest) ->
+      Printf.sprintf "Warning %d [%s]%s" n name rest
+
+let expand_names s =
+  String.split_lines s
+  |> List.map ~f:expand_names_on_line
+  |> List.map ~f:(fun s -> s ^ "\n")
+  |> String.concat ~sep:""
+
 let print_warning l w =
-  !Location.warning_printer l Caml.Format.err_formatter w
+  print_to_string (fun ppf -> !Location.warning_printer l ppf w)
+  |> expand_names
+  |> Caml.Format.eprintf "%s"
 
 let is_unexpected_docstring = function
   | Warnings.Bad_docstring _ -> true
