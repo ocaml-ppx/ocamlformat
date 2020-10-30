3a39f9e7b788fd7344fe23651f0ae4fe7553e2af
Etienne Millon <me@emillon.org>
2020-10-30 16:47:59 +0100

    Rewrite warning numbers in warning filters too (#1518)
    Followup for #1511

--- vendor/compat/warning/ge_408.ml
+++ vendor/compat/warning/ge_408.ml
@@ -1,17 +1,5 @@
 let () = Clflags.error_style := Some Misc.Error_style.Short
 
-let with_warning_filter ~filter ~f =
-  let warning_reporter = !Location.warning_reporter in
-  (Location.warning_reporter :=
-     fun loc warn ->
-       if filter loc warn then Location.default_warning_reporter loc warn
-       else None) ;
-  let reset () = Location.warning_reporter := warning_reporter in
-  try
-    let x = f () in
-    reset () ; x
-  with e -> reset () ; raise e
-
 let add_warning_name s =
   match int_of_string s with
   | exception _ -> s
@@ -33,13 +21,31 @@ let map_report_code report ~f =
   { report with Location.kind = map_kind_code ~f report.Location.kind
   }
 
+let report_add_warning_name report =
+  map_report_code report ~f:add_warning_name
+
 let print_warning l w =
   match Location.default_warning_reporter l w with
   | Some report ->
-      map_report_code report ~f:add_warning_name |>
+      report_add_warning_name report |>
       Location.print_report Caml.Format.err_formatter
   | None -> ()
 
 let is_unexpected_docstring = function
   | Warnings.Bad_docstring _ -> true
   | _ -> false
+
+let with_warning_filter ~filter ~f =
+  let warning_reporter = !Location.warning_reporter in
+  (Location.warning_reporter :=
+     fun loc warn ->
+       let report =
+         if filter loc warn then Location.default_warning_reporter loc warn
+         else None
+       in
+       Option.map report_add_warning_name report) ;
+  let reset () = Location.warning_reporter := warning_reporter in
+  try
+    let x = f () in
+    reset () ; x
+  with e -> reset () ; raise e
