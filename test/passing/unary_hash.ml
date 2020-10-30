dd18f706e5b0f4b9901d6a9a377dd35a82820354
Guillaume Petiot <guillaume@tarides.com>
2020-10-30 22:31:46 +0700

    Add tests for unary operators containing '#' from 4.12

---
+++ test/passing/unary_hash.ml
@@ -0,0 +1,19 @@
+let f o x = o##x
+
+let f x = !#x
+
+let f x = ?#x
+
+let f x = ~#x
+
+let f o x = o#-#x
+
+let f x = !-#x
+
+let f x = ?-#x
+
+let f x = ~-#x
+
+let f x = ?#(x - y)
+
+let f x = x + ?#(x + y)
