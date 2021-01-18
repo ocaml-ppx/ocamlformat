ocaml-ppx/ocamlformat

commit dce10205f9866b9eea7105d5c4045d3a5d34ccba

Cleanup string and char escapes (#1491)
OCamlformat has to deal with escaping in strings in two cases

- when `escape-strings` is set to anything other than `preserve` (this
  has been removed in #1462)
- when `Source.string_literal` cannot recover the original string in the
  source code. In that case, a decimal escape is used.

This PR is based on the observation that the latter case is never used.
It seems to never actually be the case (in the test suite and on
`test_branch`, including with older compilers, ). So we can make
`Source.string_literal` total and delete the corresponding dead code.

In technical terms, the new assumption is that the tokens found in
`Pconst_string(_, _, None)` (double quoted strings) start by a `STRING`
token.

Once we require a OCaml >= 4.11 parser we can use the payload loc
directly.

The char situation is similar but with less dead code associated.

lib/Fmt_ast.ml
@@ -233,73 +233,24 @@ let fmt_str_loc c ?pre {txt; loc} = Cmts.fmt c loc (fmt_opt pre $ str txt)
 let fmt_str_loc_opt c ?pre ?(default = "_") {txt; loc} =
   Cmts.fmt c loc (fmt_opt pre $ str (Option.value ~default txt))
 
-let char_escaped c ~loc chr =
-  Option.value (Source.char_literal c.source loc) ~default:(Char.escaped chr)
-
-let escape_string mode str =
-  match mode with
-  | `Hexadecimal ->
-      let buf = Bytes.create (4 * String.length str) in
-      for i = 0 to String.length str - 1 do
-        let src =
-          Bytes.of_string (Printf.sprintf "\\x%02x" (Char.to_int str.[i]))
-        in
-        Bytes.blit ~dst:buf ~dst_pos:(i * 4) ~src ~src_pos:0 ~len:4
-      done ;
-      Bytes.to_string buf
-  | `Preserve -> str
-  | `Decimal ->
-      let n = ref 0 in
-      for i = 0 to String.length str - 1 do
-        let l =
-          match str.[i] with
-          | '"' | '\\' | '\n' | '\t' | '\r' | '\b' -> 2
-          | ' ' .. '~' -> 1
-          | _ -> 4
-        in
-        n := !n + l
-      done ;
-      if !n = String.length str then str
-      else
-        let buf = Bytes.create !n in
-        n := 0 ;
-        let set c = Bytes.set buf !n c ; Int.incr n in
-        for i = 0 to String.length str - 1 do
-          let chr = str.[i] in
-          match chr with
-          | '"' | '\\' -> set '\\' ; set chr
-          | '\n' -> set '\\' ; set 'n'
-          | '\t' -> set '\\' ; set 't'
-          | '\r' -> set '\\' ; set 'r'
-          | '\b' -> set '\\' ; set 'b'
-          | ' ' .. '~' -> set chr
-          | _ ->
-              let code = Char.to_int chr in
-              set '\\' ;
-              set (Char.of_int_exn (48 + (code / 100))) ;
-              set (Char.of_int_exn (48 + (code / 10 % 10))) ;
-              set (Char.of_int_exn (48 + (code % 10)))
-        done ;
-        Bytes.to_string buf
-
 let fmt_constant c ~loc ?epi const =
   Cmts.fmt c loc
   @@
   match const with
   | Pconst_integer (lit, suf) | Pconst_float (lit, suf) ->
       str lit $ opt suf char
-  | Pconst_char x -> wrap "'" "'" @@ str (char_escaped ~loc c x)
+  | Pconst_char _ -> wrap "'" "'" @@ str (Source.char_literal c.source loc)
   | Pconst_string (s, _, Some delim) ->
       wrap_k (str ("{" ^ delim ^ "|")) (str ("|" ^ delim ^ "}")) (str s)
-  | Pconst_string (s, _, None) -> (
+  | Pconst_string (_, _, None) -> (
       let delim = ["@,"; "@;"] in
       let contains_pp_commands s =
         let is_substring substring = String.is_substring s ~substring in
         List.exists delim ~f:is_substring
       in
-      let fmt_string_auto ~break_on_newlines mode s =
-        let fmt_words ~epi mode s =
-          let words = String.split (escape_string mode s) ~on:' ' in
+      let fmt_string_auto ~break_on_newlines s =
+        let fmt_words ~epi s =
+          let words = String.split s ~on:' ' in
           let fmt_word ~prev:_ curr ~next =
             match next with
             | Some "" -> str curr $ str " "
@@ -324,7 +275,7 @@ let fmt_constant c ~loc ?epi const =
               $ cbreak ~fits:("", 0, "") ~breaks:("\\", 0, "")
           in
           let epi = match next with Some _ -> noop | None -> epi in
-          fmt_words ~epi mode curr $ opt next fmt_next
+          fmt_words ~epi curr $ opt next fmt_next
         in
         let lines = String.split ~on:'\n' s in
         let lines =
@@ -347,20 +298,16 @@ let fmt_constant c ~loc ?epi const =
         | `Never -> `Preserve
         | `Auto -> `Normalize
       in
-      let s, mode =
-        match Source.string_literal c.source preserve_or_normalize loc with
-        | None -> (s, `Decimal)
-        | Some s -> (s, `Preserve)
-      in
+      let s = Source.string_literal c.source preserve_or_normalize loc in
       match c.conf.break_string_literals with
       | `Auto when contains_pp_commands s ->
           let break_on_pp_commands in_ pattern =
             String.substr_replace_all in_ ~pattern ~with_:(pattern ^ "\n")
           in
           List.fold_left delim ~init:s ~f:break_on_pp_commands
-          |> fmt_string_auto mode ~break_on_newlines:true
-      | `Auto -> fmt_string_auto mode ~break_on_newlines:false s
-      | `Never -> wrap "\"" "\"" (str (escape_string mode s)) )
+          |> fmt_string_auto ~break_on_newlines:true
+      | `Auto -> fmt_string_auto ~break_on_newlines:false s
+      | `Never -> wrap "\"" "\"" (str s) )
 
 let fmt_variance = function
   | Covariant -> str "+"

lib/Source.ml
@@ -245,8 +245,8 @@ let string_literal t mode (l : Location.t) =
        | Parser.LBRACKETATAT, _
        | Parser.LBRACKETAT, _ )
        :: _ ->
-      Some (Literal_lexer.string mode (lexbuf_from_loc t loc))
-  | _ -> None
+      Literal_lexer.string mode (lexbuf_from_loc t loc)
+  | _ -> impossible "Pconst_string is only produced by string literals"
 
 let char_literal t (l : Location.t) =
   (* the location of a [char] might include surrounding comments and
@@ -268,8 +268,8 @@ let char_literal t (l : Location.t) =
        | Parser.LBRACKETATAT, _
        | Parser.LBRACKETAT, _ )
        :: _ ->
-      Some (Literal_lexer.char (lexbuf_from_loc t loc))
-  | _ -> None
+      Literal_lexer.char (lexbuf_from_loc t loc)
+  | _ -> impossible "Pconst_char is only produced by char literals"
 
 let begins_line ?(ignore_spaces = true) t (l : Location.t) =
   let rec begins_line_ cnum =

lib/Source.mli
@@ -28,10 +28,9 @@ val string_at : t -> Lexing.position -> Lexing.position -> string
 
 val sub : t -> pos:int -> len:int -> string
 
-val string_literal :
-  t -> [`Normalize | `Preserve] -> Location.t -> string option
+val string_literal : t -> [`Normalize | `Preserve] -> Location.t -> string
 
-val char_literal : t -> Location.t -> string option
+val char_literal : t -> Location.t -> string
 
 val position_before : t -> Lexing.position -> Lexing.position option
 (** [position_before s pos] returns the starting position of the token

commit c4bee36c552152b645fb371a76f2484c1a792fbe

fix bug introduced in #1491

lib/Fmt_ast.ml
@@ -254,7 +254,7 @@ let fmt_constant c ~loc ?epi const =
   | Pconst_char _ -> wrap "'" "'" @@ str (Source.char_literal c.source loc)
   | Pconst_string (s, _, Some delim) ->
       wrap_k (str ("{" ^ delim ^ "|")) (str ("|" ^ delim ^ "}")) (str s)
-  | Pconst_string (_, _, None) -> (
+  | Pconst_string (s, _, None) -> (
       let delim = ["@,"; "@;"] in
       let contains_pp_commands s =
         let is_substring substring = String.is_substring s ~substring in
@@ -310,7 +310,11 @@ let fmt_constant c ~loc ?epi const =
         | `Never -> `Preserve
         | `Auto -> `Normalize
       in
-      let s = Source.string_literal c.source preserve_or_normalize loc in
+      let s =
+        match Source.string_literal c.source preserve_or_normalize loc with
+        | exception _ -> s
+        | s -> s
+      in
       match c.conf.break_string_literals with
       | `Auto when contains_pp_commands s ->
           let break_on_pp_commands in_ pattern =