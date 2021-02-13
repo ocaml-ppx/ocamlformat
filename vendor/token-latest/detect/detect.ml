b3d3aa5d69c04e32198f640cf85d6f9756ef7333
moatom <tomozx8@gmail.com>
2021-02-13 19:25:34 +0900

    Support BER MetaOCaml N111

--- vendor/token-latest/detect/detect.ml
+++ vendor/token-latest/detect/detect.ml
@@ -1,5 +1,12 @@
 let dir_name version =
-  if version >= (4, 11) then "411"
+  if version >= (4, 11) then
+    match Sys.os_type with
+    | "Unix" -> begin  
+        match Sys.command "which metaocaml > /dev/null 2>&1" with
+        | 0 -> "411ber"
+        | _ -> "411"
+      end
+    | _ -> "411"
   else "408"
 
 let () =
