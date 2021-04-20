a58915bc8aeafeba64f77de71c9db5920f4ed8a2
Guillaume Petiot <guillaume@tarides.com>
2021-04-20 21:03:48 +0800

    forgot files

---
+++ vendor/ocamlformat-stdlib/fpath_ext.ml
@@ -0,0 +1,16 @@
+include Fpath
+
+let cwd () = Unix.getcwd () |> v
+
+let exists p = to_string p |> Caml.Sys.file_exists
+
+let to_absolute file = if is_rel file then append (cwd ()) file else file
+
+let to_string ?(relativize = false) p =
+  if relativize then
+    Base.Option.value_map
+      (Fpath.relativize ~root:(cwd ()) p)
+      ~default:(to_string p) ~f:to_string
+  else to_string p
+
+let pp fmt p = Format.fprintf fmt "%s" (to_string ~relativize:true p)
---
+++ vendor/ocamlformat-stdlib/fpath_ext.mli
@@ -0,0 +1,17 @@
+include module type of Fpath
+
+val cwd : unit -> t
+(** Current working directory, relying on [Unix]. *)
+
+val exists : t -> bool
+(** [exists p] returns whether the given path [p] exists. *)
+
+val to_absolute : t -> t
+(** [to_absolute p] returns [cwd]/[p] if the [p] is relative, otherwise
+    returns [p]. *)
+
+val to_string : ?relativize:bool -> t -> string
+(** If [relativize] is set to [true] (it is set to [false] by default), the
+    path is relativized according to the [cwd]. *)
+
+val pp : Format.formatter -> t -> unit
