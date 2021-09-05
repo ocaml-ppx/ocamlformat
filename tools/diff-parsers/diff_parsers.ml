8d4c33926c672c3422ec9c62f8945f13672185ca
Hugo Heuzard <hugo.heuzard@gmail.com>
2021-09-05 10:55:35 +0200

    wip

---
+++ tools/diff-parsers/diff_parsers.ml
@@ -0,0 +1,55 @@
+let diff d1 d2 f =
+  Sys.command
+    (Printf.sprintf "diff -U 5 %s %s" (Filename.concat d1 f)
+       (Filename.concat d2 f) )
+
+let import version dir f =
+  Sys.command
+    (Printf.sprintf
+       "curl -s -o %s \
+        https://raw.githubusercontent.com/ocaml/ocaml/%s/parsing/%s"
+       (Filename.concat dir f) version f )
+
+let files =
+  [ "ast_helper.ml"
+  ; "ast_helper.mli"
+  ; "ast_mapper.ml"
+  ; "ast_mapper.mli"
+  ; "asttypes.mli"
+  ; "docstrings.ml"
+  ; "docstrings.mli"
+  ; "lexer.mll"
+  ; "parse.ml"
+  ; "parse.mli"
+  ; "parser.mly"
+  ; "parsetree.mli"
+  ; "pprintast.ml"
+  ; "pprintast.mli" ]
+
+let usage () =
+  let exe = Filename.basename Sys.executable_name in
+  Printf.printf "usage:\n\n%s diff DIR1 DIR2\n\n%s import VERSION DIR" exe exe ;
+  exit 1
+
+let () =
+  let l = List.tl (Array.to_list Sys.argv) in
+  match l with
+  | "diff" :: args -> (
+    match args with
+    | [d1; d2] -> (
+        let codes = List.map (diff d1 d2) files in
+        let non_zero =
+          List.filter (function 0 | 1 -> false | _ -> true) codes
+        in
+        match non_zero with [] -> () | first :: _ -> exit first )
+    | _ -> usage () )
+  | "import" :: args -> (
+    match args with
+    | [version; dir] -> (
+        let codes = List.map (import version dir) files in
+        let non_zero =
+          List.filter (function 0 -> false | _ -> true) codes
+        in
+        match non_zero with [] -> () | first :: _ -> exit first )
+    | _ -> usage () )
+  | _ -> usage ()
---
+++ vendor/import-ocaml-upstream.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+dune exec ../tools/diff-parsers/diff_parsers.exe -- import 4.03 ocaml-4.13-upstream
