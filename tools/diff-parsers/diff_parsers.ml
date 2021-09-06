a7a07c3d07a387a7a3d97f83a6d475f3192ea8ff
Guillaume Petiot <guillaume@tarides.com>
2021-09-06 22:04:56 +0800

    diff ocaml-4.13-extended and parse-wyc

--- tools/diff-parsers/diff_parsers.ml
+++ tools/diff-parsers/diff_parsers.ml
@@ -1,7 +1,8 @@
 let diff d1 d2 f =
+  let f1 = Filename.concat d1 f in
+  let f2 = Filename.concat d2 f in
   Sys.command
-    (Printf.sprintf "diff -U 5 %s %s" (Filename.concat d1 f)
-       (Filename.concat d2 f) )
+    (Printf.sprintf "[ -f %s ] && diff -U 5 %s %s" f2 f1 f2)
 
 let import version dir f =
   Sys.command
