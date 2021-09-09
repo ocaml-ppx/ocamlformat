55c55ed97bf493c6660197d4558f570be562c963
Guillaume Petiot <guillaume@tarides.com>
2021-09-09 17:44:22 +0800

    check context instead

--- lib/Fmt_ast.ml
+++ lib/Fmt_ast.ml
@@ -941,11 +941,11 @@ and fmt_pattern ?ext c ?pro ?parens ?(box = false)
   let parens = match parens with Some b -> b | None -> parenze_pat xpat in
   ( match ppat_desc with
   | Ppat_or _ -> Fn.id
-  | Ppat_construct ({txt; _}, _) when Poly.(txt <> Longident.Lident "::") ->
-      let has_cmts_before = Cmts.has_before c.cmts ppat_loc in
-      fun k ->
-        hvbox_if has_cmts_before 0 @@ Cmts.fmt c ppat_loc @@ (fmt_opt pro $ k)
-  | _ -> fun k -> Cmts.fmt c ppat_loc (fmt_opt pro $ k) )
+  | _ -> (
+    match ctx0 with
+    | Pat {ppat_desc= Ppat_tuple _; _} ->
+        fun k -> hvbox 0 @@ Cmts.fmt c ppat_loc @@ (fmt_opt pro $ k)
+    | _ -> fun k -> Cmts.fmt c ppat_loc @@ (fmt_opt pro $ k) ) )
   @@ hovbox_if box 0
   @@ fmt_pattern_attributes c xpat
   @@
