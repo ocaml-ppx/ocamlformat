3c35e71577b239ffab81aeee8be2c863a2aa30da
Josh Berdine <josh@berdine.net>
2021-10-01 00:37:37 +0100

    indent comments

--- lib/Fmt_ast.ml
+++ lib/Fmt_ast.ml
@@ -1368,8 +1368,10 @@ and fmt_label_arg ?(box = true) ?epi ?parens ?eol c
   | (Labelled _ | Optional _), _ when Cmts.has_after c.cmts xarg.ast.pexp_loc
     ->
       let cmts_after = Cmts.fmt_after c xarg.ast.pexp_loc in
-      hvbox_if box 0
-        ( fmt_expression c ~pro:(fmt_label lbl ":@,") ~box ?epi ?parens xarg
+      hvbox_if box 2
+        ( hvbox 0
+            (fmt_expression c ~pro:(fmt_label lbl ":@,") ~box ?epi ?parens
+               xarg )
         $ cmts_after )
   | _ -> fmt_label lbl ":@," $ fmt_expression c ~box ?epi ?parens xarg
 
