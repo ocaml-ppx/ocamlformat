b1dc3accdea40537fd0ca4f0d49c7eb5de531aab
Guillaume Petiot <guillaume@tarides.com>
2021-10-01 14:19:32 +0100

    Bring vendored Format lib up-to-date with upstream stdlib and track differences (#1833)

--- tools/diff-ocaml/diff_ocaml.ml
+++ tools/diff-ocaml/diff_ocaml.ml
@@ -19,14 +19,13 @@ let diff d1 d2 f =
          f2 )
   else 0
 
-let import version dir f =
+let import version ~src ~dst f =
   Sys.command
     (Printf.sprintf
-       "curl -s -o %s \
-        https://raw.githubusercontent.com/ocaml/ocaml/%s/parsing/%s"
-       (Filename.concat dir f) version f )
+       "curl -s -o %s https://raw.githubusercontent.com/ocaml/ocaml/%s/%s/%s"
+       (Filename.concat dst f) version src f )
 
-let files =
+let parser_files =
   [ "ast_helper.ml"
   ; "ast_helper.mli"
   ; "ast_mapper.ml"
@@ -42,28 +41,37 @@ let files =
   ; "pprintast.ml"
   ; "pprintast.mli" ]
 
+let stdlib_files = ["format.ml"; "format.mli"]
+
 let usage () =
   let exe = Filename.basename Sys.executable_name in
-  Printf.printf "usage:\n\n%s diff DIR1 DIR2\n\n%s import VERSION DIR" exe
-    exe ;
+  Printf.printf
+    "usage:\n\n\
+     %s diff parser DIR1 DIR2\n\n\
+     %s diff stdlib DIR1 DIR2\n\n\
+     %s import VERSION DIR\n"
+    exe exe exe ;
   exit 1
+
+let diff files d1 d2 =
+  let codes = List.map (diff d1 d2) files in
+  List.iter (function 0 | 1 -> () | c -> exit c) codes
 
 let () =
   let l = List.tl (Array.to_list Sys.argv) in
   match l with
   | "diff" :: args -> (
     match args with
-    | [d1; d2] -> (
-        let codes = List.map (diff d1 d2) files in
-        let non_zero =
-          List.filter (function 0 | 1 -> false | _ -> true) codes
-        in
-        match non_zero with [] -> () | first :: _ -> exit first )
+    | ["parser"; d1; d2] -> diff parser_files d1 d2
+    | ["stdlib"; d1; d2] -> diff stdlib_files d1 d2
     | _ -> usage () )
   | "import" :: args -> (
     match args with
-    | [version; dir] -> (
-        let codes = List.map (import version dir) files in
+    | [version; dst] -> (
+        let codes =
+          List.map (import ~src:"parsing" version ~dst) parser_files
+          @ List.map (import ~src:"stdlib" version ~dst) stdlib_files
+        in
         let non_zero =
           List.filter (function 0 -> false | _ -> true) codes
         in
