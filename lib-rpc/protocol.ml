03090ace5fe88e8fc26f3445a5f2f56ba2c546bf
Guillaume Petiot <guillaume@tarides.com>
2022-03-08 14:31:18 +0000

    Add missing headers

--- lib-rpc/protocol.ml
+++ lib-rpc/protocol.ml
@@ -1,3 +1,14 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                              OCamlFormat                               *)
+(*                                                                        *)
+(*            Copyright (c) Facebook, Inc. and its affiliates.            *)
+(*                                                                        *)
+(*      This source code is licensed under the MIT license found in       *)
+(*      the LICENSE file in the root directory of this source tree.       *)
+(*                                                                        *)
+(**************************************************************************)
+
 type format_args =
   {path: string option; config: (string * string) list option}
 
