0dd1966d3fbe67d39b113839cae6f02440d52049
Guillaume Petiot <guillaume@tarides.com>
2022-06-29 19:59:21 +0100

    Preserve normalization of cons-patterns into bracket lists

--- test/passing/tests/list_normalized.ml
+++ test/passing/tests/list_normalized.ml
@@ -8,3 +8,13 @@ let x = (* a *) 1 (* b *) :: (* c *) 2 :: 3 :: 4 (* d *) :: (* e *) ([][@attr]) 
 
 (* comments preserved when no normalization required *)
 let x = (* a *) [ (* b *) 1 (* c *) ; (* d *) 2 ; 3 ; 4 (* e *) ] (* f *)
+
+let x :: [] = e
+let x :: y = e
+let x :: y :: [] = e
+let x :: y :: ([][@attr]) = e
+let x :: (y[@attr]) :: [] = e
+let (*a*)x(*b*) :: (*c*)y(*d*) = e
+let (*a*)x(*b*) :: (*c*)y(*d*) :: (*e*)[](*f*) = e
+let (*a*)x(*b*) :: (*c*)y(*d*) :: (*e*)([][@attr])(*f*) = e
+let (*a*)x(*b*) :: (*c*)(y[@attr])(*d*) :: (*e*)[](*f*) = e
