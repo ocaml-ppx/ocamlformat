35341370a7de1d6150d75d8236e0bd3e2eeb6a58
Josh Berdine <josh@berdine.net>
2022-11-07 21:24:13 +0000

    Fix file headers for lib/Conf_t.ml{,i}

--- lib/Conf_t.ml
+++ lib/Conf_t.ml
@@ -1,3 +1,14 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                              OCamlFormat                               *)
+(*                                                                        *)
+(*            Copyright (c) Facebook, Inc. and its affiliates.            *)
+(*                                                                        *)
+(*      This source code is licensed under the MIT license found in       *)
+(*      the LICENSE file in the root directory of this source tree.       *)
+(*                                                                        *)
+(**************************************************************************)
+
 type parsed_from = [`File of Location.t | `Attribute of Location.t]
 
 type updated_from = [`Env | `Commandline | `Parsed of parsed_from]
--- lib/Conf_t.mli
+++ lib/Conf_t.mli
@@ -1,3 +1,14 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                              OCamlFormat                               *)
+(*                                                                        *)
+(*            Copyright (c) Facebook, Inc. and its affiliates.            *)
+(*                                                                        *)
+(*      This source code is licensed under the MIT license found in       *)
+(*      the LICENSE file in the root directory of this source tree.       *)
+(*                                                                        *)
+(**************************************************************************)
+
 (** Configuration options *)
 
 type parsed_from = [`File of Location.t | `Attribute of Location.t]
