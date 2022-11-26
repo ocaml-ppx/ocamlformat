8291fb84d38969796cddace50b91ae5bb23ac163
hadesragnarokclassic <hadesragnarokclassic@gmail.com>
2022-11-26 14:00:32 +0700

    Revert "Merge branch 'v0.11.0'"
    This reverts commit 522de30f7326b2fdb5be3ddfa742597f6708158a, reversing
    changes made to 0dbe13ed89f43e88972bc6658f1cb36700207c23.

--- lib/Version.ml
+++ lib/Version.ml
@@ -1,1 +1,25 @@
-let version = "0.11.0"
+(**************************************************************************)
+(*                                                                        *)
+(*                              OCamlFormat                               *)
+(*                                                                        *)
+(*            Copyright (c) Facebook, Inc. and its affiliates.            *)
+(*                                                                        *)
+(*      This source code is licensed under the MIT license found in       *)
+(*      the LICENSE file in the root directory of this source tree.       *)
+(*                                                                        *)
+(**************************************************************************)
+
+type t = {major: int; minor: int; patch: int option}
+
+let make ~major ~minor ~patch = {major; minor; patch}
+
+let to_string = function
+  | {major; minor; patch= None} -> Format.sprintf "%i.%i" major minor
+  | {major; minor; patch= Some patch} ->
+      Format.sprintf "%i.%i.%i" major minor patch
+
+let pp fs v = Format.fprintf fs "%s" (to_string v)
+
+let current =
+  let open Build_info.V1 in
+  version () |> Option.value_map ~f:Version.to_string ~default:"unknown"
