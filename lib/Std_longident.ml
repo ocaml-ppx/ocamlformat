380abb390909cf549ee810e4b7d04e7224b99934
Guillaume Petiot <guillaume@tarides.com>
2022-12-05 17:34:26 +0000

    Add missing header

--- lib/Std_longident.ml
+++ lib/Std_longident.ml
@@ -1,3 +1,14 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                              OCamlFormat                               *)
+(*                                                                        *)
+(*            Copyright (c) Facebook, Inc. and its affiliates.            *)
+(*                                                                        *)
+(*      This source code is licensed under the MIT license found in       *)
+(*      the LICENSE file in the root directory of this source tree.       *)
+(*                                                                        *)
+(**************************************************************************)
+
 open Longident
 
 module Indexing_op = struct
--- lib/Std_longident.mli
+++ lib/Std_longident.mli
@@ -1,3 +1,14 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                              OCamlFormat                               *)
+(*                                                                        *)
+(*            Copyright (c) Facebook, Inc. and its affiliates.            *)
+(*                                                                        *)
+(*      This source code is licensed under the MIT license found in       *)
+(*      the LICENSE file in the root directory of this source tree.       *)
+(*                                                                        *)
+(**************************************************************************)
+
 module String_id : sig
   val is_prefix : string -> bool
   (** Holds for prefix symbols. *)
