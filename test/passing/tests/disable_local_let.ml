a401d1238581a50f239ff88f023ae2a6ad4f14ff
Guillaume Petiot <guillaume@tarides.com>
2023-10-16 11:41:09 +0800

    Add tests

--- test/passing/tests/disable_local_let.ml
+++ test/passing/tests/disable_local_let.ml
@@ -7,3 +7,28 @@ let f () =
   let x = y [@@ocamlformat "disable"]
   in
   ()
+
+let f () =
+  let open [@ocamlformat "disable"] X
+  in
+  ()
+
+let f () =
+  let module [@ocamlformat "disable"] X = Y
+  in
+  ()
+
+let f () =
+  let exception [@ocamlformat "disable"] X
+  in
+  ()
+
+class c =
+  let open [@ocamlformat "disable"] X
+  in
+  x
+
+class type c =
+  let open [@ocamlformat "disable"] X
+  in
+  x
