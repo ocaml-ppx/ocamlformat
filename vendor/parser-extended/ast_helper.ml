fe26a6cac23276ec60bd8865b67007afe70c2d5d
Guillaume Petiot <guillaume@tarides.com>
2023-12-20 15:18:17 +0800

    Use type function_param in Pcl_fun

--- vendor/parser-extended/ast_helper.ml
+++ vendor/parser-extended/ast_helper.ml
@@ -280,7 +280,7 @@ module Cl = struct
 
   let constr ?loc ?attrs a b = mk ?loc ?attrs (Pcl_constr (a, b))
   let structure ?loc ?attrs a = mk ?loc ?attrs (Pcl_structure a)
-  let fun_ ?loc ?attrs a b c d = mk ?loc ?attrs (Pcl_fun (a, b, c, d))
+  let fun_ ?loc ?attrs a b = mk ?loc ?attrs (Pcl_fun (a, b))
   let apply ?loc ?attrs a b = mk ?loc ?attrs (Pcl_apply (a, b))
   let let_ ?loc ?attrs a b = mk ?loc ?attrs (Pcl_let (a, b))
   let constraint_ ?loc ?attrs a b = mk ?loc ?attrs (Pcl_constraint (a, b))
