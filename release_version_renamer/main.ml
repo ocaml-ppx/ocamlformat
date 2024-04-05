1c312e95ef8280d917c7bd739164b8faa86a1524
Emile Trotignon <emile@tarides.com>
2024-04-05 21:30:50 +0200

    do not use new stdlib modules for compat

--- release_version_renamer/main.ml
+++ release_version_renamer/main.ml
@@ -1,10 +1,13 @@
 let handle_arg arg =
   print_endline arg ;
   let b = Buffer.create 128 in
-  In_channel.with_open_text arg (fun input ->
-      let lexbuf = Lexing.from_channel input in
-      Lexer.disabled (Buffer.add_string b) lexbuf ) ;
-  Out_channel.with_open_text arg (fun out -> Buffer.output_buffer out b)
+  let in_chan = open_in arg in
+  let lexbuf = Lexing.from_channel in_chan in
+  Lexer.disabled (Buffer.add_string b) lexbuf ;
+  close_in in_chan ;
+  let out_chan = open_out arg in
+  Buffer.output_buffer out_chan b ;
+  close_out out_chan
 
 let () =
   let argv = Sys.argv in
