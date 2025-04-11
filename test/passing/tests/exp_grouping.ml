6e00c9b0564e498747f42f6648d245198f638a84
Emile Trotignon <emile.trotignon@gmail.com>
2025-04-11 15:43:59 +0200

    add test

--- test/passing/tests/exp_grouping.ml
+++ test/passing/tests/exp_grouping.ml
@@ -449,12 +449,20 @@ let _ =
     end
     ~last ~args
 
-
 let _ =
   f ~aaaaaaaaaaaaaaaaaaaaaaaaaa ~bbbbbbbbbbbbbbbbbbbbbbb ~ccccccccccccccccccccc
     ~label:begin match x aaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa with
       | A -> aaaaaaaaaaaa
       | B -> bbbbbbbbbbbbbbbbb b bbbbbbbbbbbbbbbbb
       | C -> c cccccccccccccccccc cccccccccccccccccccccccccccccc
+    end
+    ~last ~args
+
+let _ =
+  f ~aaaaaaaaaaaaaaaaaaaaaaaaaa ~bbbbbbbbbbbbbbbbbbbbbbb ~ccccccccccccccccccccc
+    ~label:begin
+      function_ body;
+      force breakkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk;
+      return value
     end
     ~last ~args
